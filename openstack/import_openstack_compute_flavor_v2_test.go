d27b637add58d2d7a0197318f96d66157f6209fe
Joe Topjian <joe@topjian.net>
2017-08-28 17:36:00 +0000

    Compute v2: Add import test for openstack_compute_flavor_v2

---
+++ openstack/import_openstack_compute_flavor_v2_test.go
@@ -0,0 +1,34 @@
+package openstack
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccComputeV2Flavor_importBasic(t *testing.T) {
+	resourceName := "openstack_compute_flavor_v2.flavor_1"
+	var projectName = fmt.Sprintf("ACCPTTEST-%s", acctest.RandString(5))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckAdminOnly(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeV2FlavorDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeV2Flavor_basic(projectName),
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
