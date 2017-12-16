427b91b1b229bc307176b7344bbadaef6ea0dc5b
Joe Topjian <joe@topjian.net>
2017-12-16 17:13:57 +0000

    Identity v3: Token Data Source
    This commit adds the `openstack_identity_token_v3` data source.
    It can be used to obtain authentication information about the
    current token in use. This can be used as self-discovery or
    introspection of the username or project name currently in use.

---
+++ openstack/data_source_openstack_identity_token_v3_test.go
@@ -0,0 +1,53 @@
+package openstack
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccOpenStackIdentityTokenV3DataSource_basic(t *testing.T) {
+	userName := os.Getenv("OS_USERNAME")
+	projectName := os.Getenv("OS_PROJECT_NAME")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccOpenStackIdentityTokenV3DataSource_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityTokenV3DataSourceID("data.openstack_identity_token_v3.token"),
+					resource.TestCheckResourceAttr(
+						"data.openstack_identity_token_v3.token", "user_name", userName),
+					resource.TestCheckResourceAttr(
+						"data.openstack_identity_token_v3.token", "project_name", projectName),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIdentityTokenV3DataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find token data source: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Token data source ID not set")
+		}
+
+		return nil
+	}
+}
+
+const testAccOpenStackIdentityTokenV3DataSource_basic = `
+data "openstack_identity_token_v3" "token" {
+	name = "my_token"
+}
+`
