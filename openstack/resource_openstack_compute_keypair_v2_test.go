d1c4390ca0040c9b719f50f690ed49fa23f3b564
Joe Topjian <joe@topjian.net>
2018-02-06 20:49:53 +0100

    Compute v2: Generate a keypair when no public key is specified (#217)
    * Compute v2: Generate a keypair when no public key is specified.
    
    This commit will allow a public/private keypair to be generated
    when no public key is specified.
    
    * Ignoring private_key on import test
    
    * Fixing doc

--- openstack/resource_openstack_compute_keypair_v2_test.go
+++ openstack/resource_openstack_compute_keypair_v2_test.go
@@ -2,6 +2,7 @@ package openstack
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
@@ -22,6 +23,31 @@ func TestAccComputeV2Keypair_basic(t *testing.T) {
 				Config: testAccComputeV2Keypair_basic,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckComputeV2KeypairExists("openstack_compute_keypair_v2.kp_1", &keypair),
+				),
+			},
+		},
+	})
+}
+
+func TestAccComputeV2Keypair_generatePrivate(t *testing.T) {
+	var keypair keypairs.KeyPair
+
+	fingerprintRe := regexp.MustCompile(`[a-f0-9:]+`)
+	privateKeyRe := regexp.MustCompile(`.*BEGIN RSA PRIVATE KEY.*`)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeV2KeypairDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeV2Keypair_generatePrivate,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckComputeV2KeypairExists("openstack_compute_keypair_v2.kp_1", &keypair),
+					resource.TestMatchResourceAttr(
+						"openstack_compute_keypair_v2.kp_1", "fingerprint", fingerprintRe),
+					resource.TestMatchResourceAttr(
+						"openstack_compute_keypair_v2.kp_1", "private_key", privateKeyRe),
 				),
 			},
 		},
@@ -85,5 +111,11 @@ const testAccComputeV2Keypair_basic = `
 resource "openstack_compute_keypair_v2" "kp_1" {
   name = "kp_1"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAjpC1hwiOCCmKEWxJ4qzTTsJbKzndLo1BCz5PcwtUnflmU+gHJtWMZKpuEGVi29h0A/+ydKek1O18k10Ff+4tyFjiHDQAT9+OfgWf7+b1yK+qDip3X1C0UPMbwHlTfSGWLGZquwhvEFx9k3h/M+VtMvwR1lJ9LUyTAImnNjWG7TAIPmui30HvM2UiFEmqkr4ijq45MyX2+fLIePLRIFuu1p4whjHAQYufqyno3BS48icQb4p6iVEZPo4AE2o9oIyQvj2mx4dk5Y8CgSETOZTYDOR3rU2fZTRDRgPJDH9FWvQjF5tA0p3d9CoWWd2s6GKKbfoUIi8R/Db1BSPJwkqB jrp-hp-pc"
+}
+`
+
+const testAccComputeV2Keypair_generatePrivate = `
+resource "openstack_compute_keypair_v2" "kp_1" {
+  name = "kp_1"
 }
 `
