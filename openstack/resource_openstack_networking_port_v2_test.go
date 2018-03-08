5ae4cb87a0a813d1b16da4b87b62c6081613ea88
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-03-08 18:29:22 +0300

    Networking V2: update DHCP port opts tests
    Update fixtures for ports extra DHCP opts. Set a fixed ip and add subnet
    and network resource for the update fixture.

--- openstack/resource_openstack_networking_port_v2_test.go
+++ openstack/resource_openstack_networking_port_v2_test.go
@@ -1598,6 +1598,11 @@ resource "openstack_networking_port_v2" "port_1" {
   admin_state_up = "true"
   network_id = "${openstack_networking_network_v2.network_1.id}"
 
+  fixed_ip {
+    subnet_id =  "${openstack_networking_subnet_v2.subnet_1.id}"
+    ip_address = "192.168.199.23"
+  }
+
   extra_dhcp_opts {
     opt_name = "optionA"
     opt_value = "valueA"
@@ -1611,14 +1616,31 @@ resource "openstack_networking_port_v2" "port_1" {
 `
 
 const testAccNetworkingV2Port_updateExtraDHCPOpts = `
+resource "openstack_networking_network_v2" "network_1" {
+  name = "network_1"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "subnet_1" {
+  name = "subnet_1"
+  cidr = "192.168.199.0/24"
+  ip_version = 4
+  network_id = "${openstack_networking_network_v2.network_1.id}"
+}
+
 resource "openstack_networking_port_v2" "port_1" {
   name = "port_1"
   admin_state_up = "true"
   network_id = "${openstack_networking_network_v2.network_1.id}"
 
+  fixed_ip {
+    subnet_id =  "${openstack_networking_subnet_v2.subnet_1.id}"
+    ip_address = "192.168.199.23"
+  }
+
   extra_dhcp_opts {
-    opt_name = "optionA"
-    opt_value = "valueA"
+    opt_name = "optionC"
+    opt_value = "valueC"
   }
 }
 `
