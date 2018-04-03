d15cac1f8a235b58d8c9f3ffa2cbf2cc389df694
Daniel Gonzalez <d.gonzalez.nothnagel@gmail.com>
2018-04-03 16:57:05 +0200

    Add project data source (#251)
    * Add project data source
    
    * Add tests and docs for project data source

---
+++ openstack/data_source_openstack_identity_project_v3_test.go
@@ -0,0 +1,76 @@
+package openstack
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccOpenStackIdentityV3ProjectDataSource_basic(t *testing.T) {
+	projectName := fmt.Sprintf("tf_test_%s", acctest.RandString(5))
+	projectDescription := acctest.RandString(20)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckAdminOnly(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccOpenStackIdentityProjectV3DataSource_project(projectName, projectDescription),
+			},
+			resource.TestStep{
+				Config: testAccOpenStackIdentityProjectV3DataSource_basic(projectName, projectDescription),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityV3ProjectDataSourceID("data.openstack_identity_project_v3.project_1"),
+					resource.TestCheckResourceAttr(
+						"data.openstack_identity_project_v3.project_1", "name", projectName),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_project_v3.project_1", "description", projectDescription),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_project_v3.project_1", "enabled", "true"),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_project_v3.project_1", "is_domain", "false"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIdentityV3ProjectDataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find project data source: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Project data source ID not set")
+		}
+
+		return nil
+	}
+}
+
+func testAccOpenStackIdentityProjectV3DataSource_project(name, description string) string {
+	return fmt.Sprintf(`
+	resource "openstack_identity_project_v3" "project_1" {
+	  name = "%s"
+	  description = "%s"
+	}
+`, name, description)
+}
+
+func testAccOpenStackIdentityProjectV3DataSource_basic(name, description string) string {
+	return fmt.Sprintf(`
+	%s
+
+	data "openstack_identity_project_v3" "project_1" {
+      name = "${openstack_identity_project_v3.project_1.name}"
+	}
+`, testAccOpenStackIdentityProjectV3DataSource_project(name, description))
+}
