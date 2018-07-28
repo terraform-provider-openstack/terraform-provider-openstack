59fe1a26c76f1ace20d25c3233b676526b2d626d
Flavio Percoco <flaper87@gmail.com>
2018-07-28 16:46:08 +0200

    Add data openstack_identity_endpoint_v3 (#377)
    * Add data openstack_identity_endpoint_v3
    
    Allow for getting endpoint's data from the catalog. This is useful to
    get public URL's that needed for inter-service communication in the
    cloud.
    
    * Address review comments and improve service_name filtering
    
    * Add documentation

--- openstack/provider.go
+++ openstack/provider.go
@@ -198,6 +198,7 @@ func Provider() terraform.ResourceProvider {
 			"openstack_identity_project_v3":      dataSourceIdentityProjectV3(),
 			"openstack_identity_user_v3":         dataSourceIdentityUserV3(),
 			"openstack_identity_auth_scope_v3":   dataSourceIdentityAuthScopeV3(),
+			"openstack_identity_endpoint_v3":     dataSourceIdentityEndpointV3(),
 			"openstack_images_image_v2":          dataSourceImagesImageV2(),
 			"openstack_networking_network_v2":    dataSourceNetworkingNetworkV2(),
 			"openstack_networking_subnet_v2":     dataSourceNetworkingSubnetV2(),
