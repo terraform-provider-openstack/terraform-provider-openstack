8e5327b06c88f535732e77b060958a8bdc2b44f9
Schlotter, Christian (415) <christian.schlotter@daimler.com>
2018-08-07 22:39:52 +0200

    Add firewall policy data source

--- openstack/provider.go
+++ openstack/provider.go
@@ -188,6 +188,7 @@ func Provider() terraform.ResourceProvider {
 			"openstack_compute_flavor_v2":        dataSourceComputeFlavorV2(),
 			"openstack_compute_keypair_v2":       dataSourceComputeKeypairV2(),
 			"openstack_dns_zone_v2":              dataSourceDNSZoneV2(),
+			"openstack_fw_policy_v1":             dataSourceFWPolicyV1(),
 			"openstack_identity_role_v3":         dataSourceIdentityRoleV3(),
 			"openstack_identity_project_v3":      dataSourceIdentityProjectV3(),
 			"openstack_identity_user_v3":         dataSourceIdentityUserV3(),
