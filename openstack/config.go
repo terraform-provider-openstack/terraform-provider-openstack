8fb9a911a29e946df6d0dba4ab221dabaf1d9e14
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-08-12 11:24:06 +0300

    Config: add containerInfraV1Client() method
    Add new provider config's method to initialize the OpenStack Container
    Infra V1 service client.

--- openstack/config.go
+++ openstack/config.go
@@ -292,6 +292,13 @@ func (c *Config) databaseV1Client(region string) (*gophercloud.ServiceClient, er
 	})
 }
 
+func (c *Config) containerInfraV1Client(region string) (*gophercloud.ServiceClient, error) {
+	return openstack.NewContainerInfraV1(c.OsClient, gophercloud.EndpointOpts{
+		Region:       c.determineRegion(region),
+		Availability: c.getEndpointType(),
+	})
+}
+
 func (c *Config) getEndpointType() gophercloud.Availability {
 	if c.EndpointType == "internal" || c.EndpointType == "internalURL" {
 		return gophercloud.AvailabilityInternal
