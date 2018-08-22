190ec13eb0f04dba564ca3bc10abc7423263deba
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-08-23 00:03:09 +0300

    ContainerInfraV1: fix template tests formatting
    Fix formatting of cluster templates tests.

--- openstack/resource_openstack_containerinfra_clustertemplate_v1_test.go
+++ openstack/resource_openstack_containerinfra_clustertemplate_v1_test.go
@@ -127,24 +127,24 @@ func testAccCheckContainerInfraV1ClusterTemplateDestroy(s *terraform.State) erro
 func testAccContainerInfraV1ClusterTemplateBasic(clusterTemplateName, imageName string) string {
 	return fmt.Sprintf(`
 resource "openstack_images_image_v2" "%s" {
-	name   = "%s"
-	image_source_url = "https://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img"
-	container_format = "bare"
-	disk_format = "raw"
+  name             = "%s"
+  image_source_url = "https://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img"
+  container_format = "bare"
+  disk_format      = "raw"
   properties {
     os_distro = "fedora-atomic"
-  }
-
-	timeouts {
-		create = "10m"
 	}
+
+  timeouts {
+    create = "10m"
+  }
 }
 
 resource "openstack_containerinfra_clustertemplate_v1" "%s" {
-  name = "%s"
-  image = "${openstack_images_image_v2.%s.id}"
-	coe = "kubernetes"
-	http_proxy = "127.0.0.1:8801"
+  name       = "%s"
+  image      = "${openstack_images_image_v2.%s.id}"
+  coe        = "kubernetes"
+  http_proxy = "127.0.0.1:8801"
 }
 `, imageName, imageName, clusterTemplateName, clusterTemplateName, imageName)
 }
@@ -155,7 +155,7 @@ resource "openstack_images_image_v2" "%s" {
   name   = "%s"
   image_source_url = "https://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img"
   container_format = "bare"
-	disk_format = "raw"
+  disk_format = "raw"
   properties {
     os_distro = "fedora-atomic"
   }
@@ -178,10 +178,10 @@ resource "openstack_containerinfra_clustertemplate_v1" "%s" {
 func testAccContainerInfraV1ClusterTemplateLabels(clusterTemplateName, imageName string) string {
 	return fmt.Sprintf(`
 resource "openstack_images_image_v2" "%s" {
-  name   = "%s"
+  name             = "%s"
   image_source_url = "https://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img"
   container_format = "bare"
-  disk_format = "raw"
+  disk_format      = "raw"
   properties {
     os_distro = "fedora-atomic"
   }
@@ -192,15 +192,15 @@ resource "openstack_images_image_v2" "%s" {
 }
 
 resource "openstack_containerinfra_clustertemplate_v1" "%s" {
-  name = "%s"
-  image = "${openstack_images_image_v2.%s.id}"
-  coe = "kubernetes"
-  labels = {
-		kube_tag = "1.11.1"
-		prometheus_monitoring = "true"
-		influx_grafana_dashboard_enabled = "true"
-		kube_dashboard_enabled = "true"
-	}
+  name   = "%s"
+  image  = "${openstack_images_image_v2.%s.id}"
+  coe    = "kubernetes"
+  labels {
+    kube_tag                         = "1.11.1"
+    prometheus_monitoring            = "true"
+    influx_grafana_dashboard_enabled = "true"
+    kube_dashboard_enabled           = "true"
+  }
 }
 `, imageName, imageName, clusterTemplateName, clusterTemplateName, imageName)
 }
