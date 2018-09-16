4513c22715902dc4cf60a6055db66aa81e22fe43
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-09-16 22:52:54 +0300

    ContainerInfraV1: add cluster import test
    Add the "TestAccContainerInfraV1ClusterImport_basic".

---
+++ openstack/import_openstack_containerinfra_cluster_v1_test.go
@@ -0,0 +1,34 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccContainerInfraV1ClusterImport_basic(t *testing.T) {
+	resourceName := "openstack_containerinfra_cluster_v1.cluster_1"
+	clusterName := acctest.RandomWithPrefix("tf-acc-cluster")
+	imageName := acctest.RandomWithPrefix("tf-acc-image")
+	keypairName := acctest.RandomWithPrefix("tf-acc-keypair")
+	flavorName := acctest.RandomWithPrefix("tf-acc-flavor")
+	clusterTemplateName := acctest.RandomWithPrefix("tf-acc-clustertemplate")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckContainerInfra(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckContainerInfraV1ClusterDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccContainerInfraV1ClusterBasic(imageName, keypairName, flavorName, clusterTemplateName, clusterName),
+			},
+			resource.TestStep{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"keypair", "cluster_template_id"},
+			},
+		},
+	})
+}
