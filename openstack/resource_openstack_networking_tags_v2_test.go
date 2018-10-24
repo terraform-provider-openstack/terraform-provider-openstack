004d6481ce49d0a73ef0234e9c27804cb70c1933
Steven Hardy <shardy@redhat.com>
2018-10-24 15:45:46 +0000

    Add tags support for Neutron floatingips
    This allows us to set/get tags for floatingips via terraform templates,
    and requires the recently added changes to gophercloud that enable
    the relevant features of the Neutron APIs to be accessed.
    
    Issue: #453

--- openstack/resource_openstack_networking_tags_v2_test.go
+++ openstack/resource_openstack_networking_tags_v2_test.go
@@ -31,6 +31,9 @@ func TestAccNetworkingV2_tags(t *testing.T) {
 					testAccCheckNetworkingV2Tags(
 						"openstack_networking_secgroup_v2.secgroup_1",
 						[]string{"a", "b", "c"}),
+					testAccCheckNetworkingV2Tags(
+						"openstack_networking_floatingip_v2.fip_1",
+						[]string{"a", "b", "c"}),
 				),
 			},
 			resource.TestStep{
@@ -50,6 +53,9 @@ func TestAccNetworkingV2_tags(t *testing.T) {
 						[]string{"a", "b", "c", "d"}),
 					testAccCheckNetworkingV2Tags(
 						"openstack_networking_secgroup_v2.secgroup_1",
+						[]string{"a", "b", "c", "d"}),
+					testAccCheckNetworkingV2Tags(
+						"openstack_networking_floatingip_v2.fip_1",
 						[]string{"a", "b", "c", "d"}),
 				),
 			},
@@ -110,6 +116,10 @@ resource "openstack_networking_secgroup_v2" "secgroup_1" {
   name = "security_group"
   description = "terraform security group acceptance test"
   tags = %[1]s
+}
+
+resource "openstack_networking_floatingip_v2" "fip_1" {
+	    tags = %[1]s
 }
 `
 
