90ac515f31a1d9db687b95ff4e3d0ff231fe0528
Joe Topjian <joe@topjian.net>
2018-11-12 19:23:14 -0700

    Compute v2: Adding fingerprint to keypair data source (#481)

--- openstack/data_source_openstack_compute_keypair_v2_test.go
+++ openstack/data_source_openstack_compute_keypair_v2_test.go
@@ -21,6 +21,8 @@ func TestAccComputeV2KeypairDataSource_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						"data.openstack_compute_keypair_v2.kp", "name", "the-key-name"),
 					resource.TestCheckResourceAttr(
+						"data.openstack_compute_keypair_v2.kp", "fingerprint", "78:a9:d0:f9:af:a8:1b:ca:bb:9f:65:88:47:af:1d:a9"),
+					resource.TestCheckResourceAttr(
 						"data.openstack_compute_keypair_v2.kp", "public_key", "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAjpC1hwiOCCmKEWxJ4qzTTsJbKzndLo1BCz5PcwtUnflmU+gHJtWMZKpuEGVi29h0A/+ydKek1O18k10Ff+4tyFjiHDQAT9+OfgWf7+b1yK+qDip3X1C0UPMbwHlTfSGWLGZquwhvEFx9k3h/M+VtMvwR1lJ9LUyTAImnNjWG7TAIPmui30HvM2UiFEmqkr4ijq45MyX2+fLIePLRIFuu1p4whjHAQYufqyno3BS48icQb4p6iVEZPo4AE2o9oIyQvj2mx4dk5Y8CgSETOZTYDOR3rU2fZTRDRgPJDH9FWvQjF5tA0p3d9CoWWd2s6GKKbfoUIi8R/Db1BSPJwkqB jrp-hp-pc"),
 				),
 			},
