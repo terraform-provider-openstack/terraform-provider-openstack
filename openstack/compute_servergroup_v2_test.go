c230efc145c902d4003a1086d9e6a16b1babf7f3
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-11-21 12:04:27 +0300

    ComputeV2: set microversion for new policies
    Set "2.15" microversion for servergroup "soft-anti-affinity" and
    "soft-affinity" policies.

--- openstack/compute_servergroup_v2_test.go
+++ openstack/compute_servergroup_v2_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/servergroups"
+	th "github.com/gophercloud/gophercloud/testhelper"
+	thclient "github.com/gophercloud/gophercloud/testhelper/client"
 )
 
 func TestComputeServerGroupV2CreateOpts(t *testing.T) {
@@ -37,6 +39,8 @@ func TestComputeServerGroupV2CreateOpts(t *testing.T) {
 }
 
 func TestExpandComputeServerGroupV2Policies(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
 	raw := []interface{}{
 		"affinity",
 	}
@@ -45,8 +49,38 @@ func TestExpandComputeServerGroupV2Policies(t *testing.T) {
 		"affinity",
 	}
 
-	actual := expandComputeServerGroupV2Policies(raw)
+	client := thclient.ServiceClient()
+	actual := expandComputeServerGroupV2Policies(client, raw)
 
+	if client.Microversion != "" {
+		t.Fatalf("Expected no microversion in client, but got %s", client.Microversion)
+	}
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("Results differ. Want: #%v, but got %#v", expected, actual)
+	}
+}
+
+func TestExpandComputeServerGroupV2PoliciesMicroversions(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	raw := []interface{}{
+		"affinity",
+		"soft-anti-affinity",
+		"soft-affinity",
+	}
+
+	expected := []string{
+		"affinity",
+		"soft-anti-affinity",
+		"soft-affinity",
+	}
+
+	client := thclient.ServiceClient()
+	actual := expandComputeServerGroupV2Policies(client, raw)
+
+	if client.Microversion != "2.15" {
+		t.Fatalf("Expected 2.15 microversion in client, but got %s", client.Microversion)
+	}
 	if !reflect.DeepEqual(expected, actual) {
 		t.Fatalf("Results differ. Want: #%v, but got %#v", expected, actual)
 	}
