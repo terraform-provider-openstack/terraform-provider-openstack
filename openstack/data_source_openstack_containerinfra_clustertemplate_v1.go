ed6378201ee460d7d792314eed36506c206b1225
Joe Topjian <joe@topjian.net>
2018-12-21 13:47:28 -0700

    Internal: Container Infra cleanup (#545)

--- openstack/data_source_openstack_containerinfra_clustertemplate_v1.go
+++ openstack/data_source_openstack_containerinfra_clustertemplate_v1.go
@@ -18,126 +18,157 @@ func dataSourceContainerInfraClusterTemplateV1() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
+
 			"name": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
+
 			"project_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"user_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"created_at": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"updated_at": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"apiserver_port": &schema.Schema{
 				Type:     schema.TypeInt,
 				Computed: true,
 			},
+
 			"coe": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"cluster_distro": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"dns_nameserver": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"docker_storage_driver": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"docker_volume_size": &schema.Schema{
 				Type:     schema.TypeInt,
 				Computed: true,
 			},
+
 			"external_network_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"fixed_network": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"fixed_subnet": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"flavor": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"master_flavor": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"floating_ip_enabled": &schema.Schema{
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
+
 			"http_proxy": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"https_proxy": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"image": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"insecure_registry": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"keypair_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"labels": &schema.Schema{
 				Type:     schema.TypeMap,
 				Computed: true,
 			},
+
 			"master_lb_enabled": &schema.Schema{
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
+
 			"network_driver": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"no_proxy": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"public": &schema.Schema{
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
+
 			"registry_enabled": &schema.Schema{
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
+
 			"server_type": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
 			"tls_disabled": &schema.Schema{
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
+
 			"volume_driver": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
@@ -156,7 +187,7 @@ func dataSourceContainerInfraClusterTemplateV1Read(d *schema.ResourceData, meta 
 	name := d.Get("name").(string)
 	ct, err := clustertemplates.Get(containerInfraClient, name).Extract()
 	if err != nil {
-		return fmt.Errorf("Error getting OpenStack container infra cluster template: %s", err)
+		return fmt.Errorf("Error getting openstack_containerinfra_clustertemplate_v1 %s: %s", name, err)
 	}
 
 	d.SetId(ct.UUID)
@@ -191,10 +222,10 @@ func dataSourceContainerInfraClusterTemplateV1Read(d *schema.ResourceData, meta 
 	d.Set("volume_driver", ct.VolumeDriver)
 
 	if err := d.Set("created_at", ct.CreatedAt.Format(time.RFC3339)); err != nil {
-		log.Printf("[DEBUG] created_at: %s", err)
+		log.Printf("[DEBUG] Unable to set openstack_containerinfra_clustertemplate_v1 created_at: %s", err)
 	}
 	if err := d.Set("updated_at", ct.UpdatedAt.Format(time.RFC3339)); err != nil {
-		log.Printf("[DEBUG] updated_at: %s", err)
+		log.Printf("[DEBUG] Unable to set openstack_containerinfra_clustertemplate_v1 updated_at: %s", err)
 	}
 
 	d.Set("region", GetRegion(d, config))
