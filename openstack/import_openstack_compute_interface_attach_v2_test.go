ea8f3766aa70eb922e47d0929ec2ad1d37c28a2f
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-12-25 21:10:29 +0300

    ComputeV2: fix interface attach importing
    Set "instance_id" attribute while reading
    openstack_compute_interface_attach_v2 resource and add basic import
    acceptance test for this resource.

---
+++ openstack/import_openstack_compute_interface_attach_v2_test.go
@@ -0,0 +1,27 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccComputeV2InterfaceAttachImport_basic(t *testing.T) {
+	resourceName := "openstack_compute_interface_attach_v2.ai_1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeV2InterfaceAttachDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeV2InterfaceAttach_basic,
+			},
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
