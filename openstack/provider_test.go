e6b75d26f70d1a10085eb8749bb783bac0454cd3
kayrus <kay.diam@gmail.com>
2019-01-15 06:53:06 +0100

    Acceptance tests: panic on schema error
    Set TF_SCHEMA_PANIC_ON_ERROR=1

--- openstack/provider_test.go
+++ openstack/provider_test.go
@@ -72,6 +72,11 @@ func testAccPreCheckRequiredEnvVars(t *testing.T) {
 	if OS_EXTGW_ID == "" {
 		t.Fatal("OS_EXTGW_ID must be set for acceptance tests")
 	}
+
+	// Openlab test environment sets only TF_ACC=1, forcing TF_SCHEMA_PANIC_ON_ERROR=1 as well
+	if os.Getenv("TF_ACC") != "" {
+		os.Setenv("TF_SCHEMA_PANIC_ON_ERROR", "1")
+	}
 }
 
 func testAccPreCheck(t *testing.T) {
