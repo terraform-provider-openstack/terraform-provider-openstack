2c18ca57660b3c75bb7666317ad3f8bf0140e813
Joe Topjian <joe@topjian.net>
2019-02-11 01:37:53 +0000

    Internal: openstack_db_user_v1 cleanup

---
+++ openstack/db_user_v1_test.go
@@ -0,0 +1,41 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/gophercloud/gophercloud/openstack/db/v1/databases"
+)
+
+func TestExpandDatabaseUserV1Databases(t *testing.T) {
+	dbs := []interface{}{"db1", "db2"}
+
+	expected := databases.BatchCreateOpts{
+		databases.CreateOpts{
+			Name: "db1",
+		},
+		databases.CreateOpts{
+			Name: "db2",
+		},
+	}
+
+	actual := expandDatabaseUserV1Databases(dbs)
+	assert.Equal(t, expected, actual)
+}
+
+func TestFlattenDatabaseUserV1Databases(t *testing.T) {
+	dbs := []databases.Database{
+		{
+			Name: "db1",
+		},
+		{
+			Name: "db2",
+		},
+	}
+
+	expected := []string{"db1", "db2"}
+
+	actual := flattenDatabaseUserV1Databases(dbs)
+	assert.Equal(t, expected, actual)
+}
