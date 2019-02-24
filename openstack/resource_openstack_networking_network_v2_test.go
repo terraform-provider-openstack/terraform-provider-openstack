2897f9172cfb2cbee1fee24d6c58b83829e84d90
Joe Topjian <joe@topjian.net>
2019-02-24 22:41:49 +0000

    Acc Tests: Fixes for v0.12

--- openstack/resource_openstack_networking_network_v2_test.go
+++ openstack/resource_openstack_networking_network_v2_test.go
@@ -445,8 +445,8 @@ resource "openstack_networking_port_v2" "port_1" {
   network_id = "${openstack_networking_network_v2.network_1.id}"
 
   fixed_ip {
-    "subnet_id" =  "${openstack_networking_subnet_v2.subnet_1.id}"
-    "ip_address" =  "192.168.199.23"
+    subnet_id =  "${openstack_networking_subnet_v2.subnet_1.id}"
+    ip_address =  "192.168.199.23"
   }
 }
 
