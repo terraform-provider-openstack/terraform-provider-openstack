517db27f1a7ead1eeb2222d6018f995ed280eefc
erickeller <keller.eric@gmail.com>
2019-03-21 02:26:04 +0100

    add multi-vm with corresponding floating ip example (#710)
    * add multi-vm with corresponding floating ip example
    
    This is an example how to spawn multiple openstack vm and automatically adding coresponding floating ips.
    
    * reword according to the PR comments
    
    * drop element usage in favor of index based notation
    
    * parametrize the instance_prefix

---
+++ examples/multiple-vm-with-floating-ip/main.tf
@@ -0,0 +1,27 @@
+resource "openstack_compute_keypair_v2" "terraform" {
+  name       = "terraform"
+  public_key = "${file("${var.ssh_key_file}.pub")}"
+}
+
+resource "openstack_compute_instance_v2" "multi" {
+  count = "${var.count}"
+  name = "${format("${var.instance_prefix}-%02d", count.index+1)}"
+  image_name = "${var.image}"
+  flavor_name = "${var.flavor}"
+  key_pair = "${openstack_compute_keypair_v2.terraform.name}"
+  security_groups = ["default"]
+  network {
+    name = "${var.network_name}"
+  }
+}
+
+resource "openstack_networking_floatingip_v2" "fip" {
+  count = "${var.count}"
+  pool = "${var.pool}"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "fip" {
+  count       = "${var.count}"
+  instance_id = "${openstack_compute_instance_v2.multi.*.id[count.index]}"
+  floating_ip = "${openstack_networking_floatingip_v2.fip.*.address[count.index]}"
+}
