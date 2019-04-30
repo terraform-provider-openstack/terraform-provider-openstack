ef379072cc9e58eeec6c42b2918c8edecfeaf102
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-04-30 13:31:53 +0300

    Internal: Networking V2 Router Route cleanup (#680)
    * Internal: Networking V2 Router Route cleanup
    
    Cleanup messages and variables definition for the
    openstack_networking_router_route_v2 resource.
    
    * Networking V2 Router Route: fix working with ID
    
    Provide locked routerID into routers.Get method.
    Don't cleanup d.Id in resource's Create because it doesn't make sense.
    
    * Networking V2 Router Route: fix Read and Delete
    
    Fix logic of the resourceNetworkingRouterRouteV2Read() by ensuring that
    we only use routerID parsed from the route ID if it's not empty.
    
    Fix logic of the resourceNetworkingRouterRouteV2Delete() by ensuring
    that we're saving all routes that differs from the one that is needed to
    be deleted.
    
    * Networking V2 Router Route: use CheckDeleted func
    
    Use CheckDeleted when we're checking the 404 status code while working
    with routers API.
    
    * Networking V2 Router Route: fix routers.Update
    
    Use locked "routerID" parameter instead of empty "d.Id()" as an argument
    for the "routers.Update()" call.
    
    * Networking V2 Router Route: fix read method
    
    Set "router_id" attribute in resourceNetworkingRouterRouteV2Read().

---
+++ openstack/networking_router_route_v2_test.go
@@ -0,0 +1,64 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResourceNetworkingRouterRouteV2BuildID(t *testing.T) {
+	expected := "d190e837-090a-44f2-adcf-07f9fd392931-route-10.11.12.0/24-192.168.0.111"
+
+	actual := resourceNetworkingRouterRouteV2BuildID(
+		"d190e837-090a-44f2-adcf-07f9fd392931",
+		"10.11.12.0/24",
+		"192.168.0.111",
+	)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestPesourceNetworkingRouterRouteV2ParseValidID(t *testing.T) {
+	routeID := "40412709-86e2-411a-a66f-16053188ed46-route-192.168.0.0/24-10.11.12.13"
+
+	expectedRouterID := "40412709-86e2-411a-a66f-16053188ed46"
+	expectedDstCIDR := "192.168.0.0/24"
+	expectedNextHop := "10.11.12.13"
+
+	actualRouterID, actualDstCIDR, actualNextHop, err := resourceNetworkingRouterRouteV2ParseID(routeID)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedRouterID, actualRouterID)
+	assert.Equal(t, expectedDstCIDR, actualDstCIDR)
+	assert.Equal(t, expectedNextHop, actualNextHop)
+}
+
+func TestPesourceNetworkingRouterRouteV2ParseIDInvalidFirstPart(t *testing.T) {
+	routeID := "123-router"
+
+	expectedRouterID := ""
+	expectedDstCIDR := ""
+	expectedNextHop := ""
+
+	actualRouterID, actualDstCIDR, actualNextHop, err := resourceNetworkingRouterRouteV2ParseID(routeID)
+
+	assert.Error(t, err, "invalid ID format: 123-router")
+	assert.Equal(t, expectedRouterID, actualRouterID)
+	assert.Equal(t, expectedDstCIDR, actualDstCIDR)
+	assert.Equal(t, expectedNextHop, actualNextHop)
+}
+
+func TestPesourceNetworkingRouterRouteV2ParseIDInvalidLastPart(t *testing.T) {
+	routeID := "123-router-bad"
+
+	expectedRouterID := ""
+	expectedDstCIDR := ""
+	expectedNextHop := ""
+
+	actualRouterID, actualDstCIDR, actualNextHop, err := resourceNetworkingRouterRouteV2ParseID(routeID)
+
+	assert.Error(t, err, "invalid last part format for 123-router-bad: bad")
+	assert.Equal(t, expectedRouterID, actualRouterID)
+	assert.Equal(t, expectedDstCIDR, actualDstCIDR)
+	assert.Equal(t, expectedNextHop, actualNextHop)
+}
