0e66a94ba54ebe79d1c8ee5b896b225360355ec3
Renat Nurgaliyev <impleman@gmail.com>
2019-05-08 17:17:25 +0200

    Compute V2: Use Delete timeout in stop_before_destroy (#750)

--- openstack/resource_openstack_compute_instance_v2.go
+++ openstack/resource_openstack_compute_instance_v2.go
@@ -897,7 +897,7 @@ func resourceComputeInstanceV2Delete(d *schema.ResourceData, meta interface{}) e
 				Pending:    []string{"ACTIVE"},
 				Target:     []string{"SHUTOFF"},
 				Refresh:    ServerV2StateRefreshFunc(computeClient, d.Id()),
-				Timeout:    3 * time.Minute,
+				Timeout:    d.Timeout(schema.TimeoutDelete),
 				Delay:      10 * time.Second,
 				MinTimeout: 3 * time.Second,
 			}
