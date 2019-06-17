d112a52de09bd8a5523cbce4e830186e6f69d9fe
Joe Topjian <joe@topjian.net>
2019-06-17 08:13:23 -0600

    Compute v2: Fix scheduler hints query (#771)
    * vendor: Updating Gophercloud
    
    * Compute v2: Fix scheduler hints query
    
    This commit fixes how the scheduler hints query is built. Previous
    behavior was appending data to a slice rather than inserting at index 0
    of the slice. Combined with a vendor update, scheduler hints queries
    now work for simple queries.

--- openstack/resource_openstack_compute_instance_v2.go
+++ openstack/resource_openstack_compute_instance_v2.go
@@ -1036,7 +1036,7 @@ func resourceInstanceSchedulerHintsV2(d *schema.ResourceData, schedulerHintsRaw 
 		}
 	}
 
-	query := make([]interface{}, len(schedulerHintsRaw["query"].([]interface{})))
+	query := []interface{}{}
 	if len(schedulerHintsRaw["query"].([]interface{})) > 0 {
 		for _, q := range schedulerHintsRaw["query"].([]interface{}) {
 			query = append(query, q.(string))
