ce4539bbb3797616a49807b5d5d253eaf877ca88
Jesper Schmitz Mouridsen <jesper@schmitz.computer>
2019-07-22 13:39:03 +0200

    Add a test case and documentation.

--- openstack/import_openstack_compute_instance_v2_test.go
+++ openstack/import_openstack_compute_instance_v2_test.go
@@ -53,3 +53,26 @@ func TestAccComputeV2Instance_importbootFromVolumeForceNew_1(t *testing.T) {
 		},
 	})
 }
+func TestAccComputeV2Instance_importbootFromVolumeImage(t *testing.T) {
+	resourceName := "openstack_compute_instance_v2.instance_1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeV2InstanceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeV2Instance_bootFromVolumeImage,
+			},
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"stop_before_destroy",
+					"force_delete",
+				},
+			},
+		},
+	})
+}
