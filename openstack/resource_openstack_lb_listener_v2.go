51571d0d4b91e8fea06851db8b07fe3f8fe35294
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-09-19 20:25:57 +0300

    LB Listeners V2: move structs into lb_v2_shared
    Move choosing of types for responses and requests opts into the
    lb_v2_shared.go from resource_openstack_lb_listener_v2.go.
    
    Update log messages.

--- openstack/resource_openstack_lb_listener_v2.go
+++ openstack/resource_openstack_lb_listener_v2.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 
-	octavialisteners "github.com/gophercloud/gophercloud/openstack/loadbalancer/v2/listeners"
 	neutronlisteners "github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/lbaas_v2/listeners"
 )
 
@@ -143,94 +142,18 @@ func resourceListenerV2Create(d *schema.ResourceData, meta interface{}) error {
 		return fmt.Errorf("Error creating OpenStack networking client: %s", err)
 	}
 
-	lbID := d.Get("loadbalancer_id").(string)
-	adminStateUp := d.Get("admin_state_up").(bool)
-	var sniContainerRefs []string
-	if raw, ok := d.GetOk("sni_container_refs"); ok {
-		for _, v := range raw.([]interface{}) {
-			sniContainerRefs = append(sniContainerRefs, v.(string))
-		}
-	}
-
 	timeout := d.Timeout(schema.TimeoutCreate)
 
 	// Wait for LoadBalancer to become active before continuing.
-	err = waitForLBV2LoadBalancer(lbClient, lbID, "ACTIVE", lbPendingStatuses, timeout)
+	err = waitForLBV2LoadBalancer(lbClient, d.Get("loadbalancer_id").(string), "ACTIVE", lbPendingStatuses, timeout)
 	if err != nil {
 		return err
 	}
 
 	// Choose either the Octavia or Neutron create options.
-	var createOpts neutronlisteners.CreateOptsBuilder
-	if config.useOctavia {
-		// Use Octavia.
-		opts := octavialisteners.CreateOpts{
-			Protocol:               octavialisteners.Protocol(d.Get("protocol").(string)),
-			ProtocolPort:           d.Get("protocol_port").(int),
-			ProjectID:              d.Get("tenant_id").(string),
-			LoadbalancerID:         lbID,
-			Name:                   d.Get("name").(string),
-			DefaultPoolID:          d.Get("default_pool_id").(string),
-			Description:            d.Get("description").(string),
-			DefaultTlsContainerRef: d.Get("default_tls_container_ref").(string),
-			SniContainerRefs:       sniContainerRefs,
-			AdminStateUp:           &adminStateUp,
-		}
+	createOpts := chooseLBV2ListenerCreateOpts(d, config)
 
-		if v, ok := d.GetOk("connection_limit"); ok {
-			connectionLimit := v.(int)
-			opts.ConnLimit = &connectionLimit
-		}
-
-		if v, ok := d.GetOk("timeout_client_data"); ok {
-			timeoutClientData := v.(int)
-			opts.TimeoutClientData = &timeoutClientData
-		}
-
-		if v, ok := d.GetOk("timeout_member_connect"); ok {
-			timeoutMemberConnect := v.(int)
-			opts.TimeoutMemberConnect = &timeoutMemberConnect
-		}
-
-		if v, ok := d.GetOk("timeout_member_data"); ok {
-			timeoutMemberData := v.(int)
-			opts.TimeoutMemberData = &timeoutMemberData
-		}
-
-		if v, ok := d.GetOk("timeout_tcp_inspect"); ok {
-			timeoutTCPInspect := v.(int)
-			opts.TimeoutTCPInspect = &timeoutTCPInspect
-		}
-
-		log.Printf("[DEBUG] Create Options: %#v", opts)
-
-		createOpts = opts
-	} else {
-		// Use Neutron.
-		opts := neutronlisteners.CreateOpts{
-			Protocol:               neutronlisteners.Protocol(d.Get("protocol").(string)),
-			ProtocolPort:           d.Get("protocol_port").(int),
-			TenantID:               d.Get("tenant_id").(string),
-			LoadbalancerID:         lbID,
-			Name:                   d.Get("name").(string),
-			DefaultPoolID:          d.Get("default_pool_id").(string),
-			Description:            d.Get("description").(string),
-			DefaultTlsContainerRef: d.Get("default_tls_container_ref").(string),
-			SniContainerRefs:       sniContainerRefs,
-			AdminStateUp:           &adminStateUp,
-		}
-
-		if v, ok := d.GetOk("connection_limit"); ok {
-			connectionLimit := v.(int)
-			opts.ConnLimit = &connectionLimit
-		}
-
-		log.Printf("[DEBUG] Create Options: %#v", opts)
-
-		createOpts = opts
-	}
-
-	log.Printf("[DEBUG] Attempting to create listener")
+	log.Printf("[DEBUG] openstack_lb_listener_v2 create options: %#v", createOpts)
 	var listener *neutronlisteners.Listener
 	err = resource.Retry(timeout, func() *resource.RetryError {
 		listener, err = neutronlisteners.Create(lbClient, createOpts).Extract()
@@ -241,7 +164,7 @@ func resourceListenerV2Create(d *schema.ResourceData, meta interface{}) error {
 	})
 
 	if err != nil {
-		return fmt.Errorf("Error creating listener: %s", err)
+		return fmt.Errorf("Error creating openstack_lb_listener_v2: %s", err)
 	}
 
 	// Wait for the listener to become ACTIVE.
@@ -262,61 +185,7 @@ func resourceListenerV2Read(d *schema.ResourceData, meta interface{}) error {
 		return fmt.Errorf("Error creating OpenStack networking client: %s", err)
 	}
 
-	listenerResp := neutronlisteners.Get(lbClient, d.Id())
-
-	// Choose either the Octavia or Neutron response type.
-	if config.useOctavia {
-		var listener octavialisteners.Listener
-		err = listenerResp.ExtractInto(listener)
-		if err != nil {
-			return CheckDeleted(d, err, "listener")
-		}
-
-		log.Printf("[DEBUG] Retrieved listener %s: %#v", d.Id(), listener)
-
-		d.Set("name", listener.Name)
-		d.Set("protocol", listener.Protocol)
-		d.Set("tenant_id", listener.ProjectID)
-		d.Set("description", listener.Description)
-		d.Set("protocol_port", listener.ProtocolPort)
-		d.Set("admin_state_up", listener.AdminStateUp)
-		d.Set("default_pool_id", listener.DefaultPoolID)
-		d.Set("connection_limit", listener.ConnLimit)
-		d.Set("timeout_client_data", listener.TimeoutClientData)
-		d.Set("timeout_member_connect", listener.TimeoutMemberConnect)
-		d.Set("timeout_member_data", listener.TimeoutMemberData)
-		d.Set("timeout_tcp_inspect", listener.TimeoutTCPInspect)
-		d.Set("sni_container_refs", listener.SniContainerRefs)
-		d.Set("default_tls_container_ref", listener.DefaultTlsContainerRef)
-		d.Set("region", GetRegion(d, config))
-	} else {
-		var listener neutronlisteners.Listener
-		err = listenerResp.ExtractInto(listener)
-		if err != nil {
-			return CheckDeleted(d, err, "listener")
-		}
-
-		log.Printf("[DEBUG] Retrieved listener %s: %#v", d.Id(), listener)
-
-		// Required by import
-		if len(listener.Loadbalancers) > 0 {
-			d.Set("loadbalancer_id", listener.Loadbalancers[0].ID)
-		}
-
-		d.Set("name", listener.Name)
-		d.Set("protocol", listener.Protocol)
-		d.Set("tenant_id", listener.TenantID)
-		d.Set("description", listener.Description)
-		d.Set("protocol_port", listener.ProtocolPort)
-		d.Set("admin_state_up", listener.AdminStateUp)
-		d.Set("default_pool_id", listener.DefaultPoolID)
-		d.Set("connection_limit", listener.ConnLimit)
-		d.Set("sni_container_refs", listener.SniContainerRefs)
-		d.Set("default_tls_container_ref", listener.DefaultTlsContainerRef)
-		d.Set("region", GetRegion(d, config))
-	}
-
-	return nil
+	return chooseLBV2ListenerReadBody(neutronlisteners.Get(lbClient, d.Id()), d, config)
 }
 
 func resourceListenerV2Update(d *schema.ResourceData, meta interface{}) error {
@@ -329,7 +198,7 @@ func resourceListenerV2Update(d *schema.ResourceData, meta interface{}) error {
 	// Get a clean copy of the listener.
 	listener, err := neutronlisteners.Get(lbClient, d.Id()).Extract()
 	if err != nil {
-		return fmt.Errorf("Unable to retrieve listener %s: %s", d.Id(), err)
+		return fmt.Errorf("Unable to retrieve openstack_lb_listener_v2 %s: %s", d.Id(), err)
 	}
 
 	// Wait for the listener to become ACTIVE.
@@ -339,101 +208,9 @@ func resourceListenerV2Update(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	// Choose either the Octavia or Neutron update options.
-	var updateOpts neutronlisteners.UpdateOptsBuilder
-	if config.useOctavia {
-		// Use Octavia.
-		var opts octavialisteners.UpdateOpts
-		if d.HasChange("name") {
-			name := d.Get("name").(string)
-			opts.Name = &name
-		}
-		if d.HasChange("description") {
-			description := d.Get("description").(string)
-			opts.Description = &description
-		}
-		if d.HasChange("connection_limit") {
-			connLimit := d.Get("connection_limit").(int)
-			opts.ConnLimit = &connLimit
-		}
-		if d.HasChange("timeout_client_data") {
-			timeoutClientData := d.Get("timeout_client_data").(int)
-			opts.ConnLimit = &timeoutClientData
-		}
-		if d.HasChange("timeout_member_connect") {
-			timeoutMemberConnect := d.Get("timeout_member_connect").(int)
-			opts.ConnLimit = &timeoutMemberConnect
-		}
-		if d.HasChange("timeout_member_data") {
-			timeoutMemberData := d.Get("timeout_member_data").(int)
-			opts.ConnLimit = &timeoutMemberData
-		}
-		if d.HasChange("timeout_tcp_inspect") {
-			timeoutTCPInspect := d.Get("timeout_tcp_inspect").(int)
-			opts.ConnLimit = &timeoutTCPInspect
-		}
-		if d.HasChange("default_pool_id") {
-			defaultPoolID := d.Get("default_pool_id").(string)
-			opts.DefaultPoolID = &defaultPoolID
-		}
-		if d.HasChange("default_tls_container_ref") {
-			opts.DefaultTlsContainerRef = d.Get("default_tls_container_ref").(string)
-		}
-		if d.HasChange("sni_container_refs") {
-			var sniContainerRefs []string
-			if raw, ok := d.GetOk("sni_container_refs"); ok {
-				for _, v := range raw.([]interface{}) {
-					sniContainerRefs = append(sniContainerRefs, v.(string))
-				}
-			}
-			opts.SniContainerRefs = sniContainerRefs
-		}
-		if d.HasChange("admin_state_up") {
-			asu := d.Get("admin_state_up").(bool)
-			opts.AdminStateUp = &asu
-		}
+	updateOpts := chooseLBV2ListenerUpdateOpts(d, config)
 
-		updateOpts = opts
-	} else {
-		// Use Neutron.
-		var opts neutronlisteners.UpdateOpts
-		if d.HasChange("name") {
-			name := d.Get("name").(string)
-			opts.Name = &name
-		}
-		if d.HasChange("description") {
-			description := d.Get("description").(string)
-			opts.Description = &description
-		}
-		if d.HasChange("connection_limit") {
-			connLimit := d.Get("connection_limit").(int)
-			opts.ConnLimit = &connLimit
-		}
-		if d.HasChange("default_pool_id") {
-			defaultPoolID := d.Get("default_pool_id").(string)
-			opts.DefaultPoolID = &defaultPoolID
-		}
-		if d.HasChange("default_tls_container_ref") {
-			opts.DefaultTlsContainerRef = d.Get("default_tls_container_ref").(string)
-		}
-		if d.HasChange("sni_container_refs") {
-			var sniContainerRefs []string
-			if raw, ok := d.GetOk("sni_container_refs"); ok {
-				for _, v := range raw.([]interface{}) {
-					sniContainerRefs = append(sniContainerRefs, v.(string))
-				}
-			}
-			opts.SniContainerRefs = sniContainerRefs
-		}
-		if d.HasChange("admin_state_up") {
-			asu := d.Get("admin_state_up").(bool)
-			opts.AdminStateUp = &asu
-		}
-
-		updateOpts = opts
-	}
-
-	log.Printf("[DEBUG] Updating listener %s with options: %#v", d.Id(), updateOpts)
+	log.Printf("[DEBUG] openstack_lb_listener_v2 %s update options: %#v", d.Id(), updateOpts)
 	err = resource.Retry(timeout, func() *resource.RetryError {
 		_, err = neutronlisteners.Update(lbClient, d.Id(), updateOpts).Extract()
 		if err != nil {
@@ -443,7 +220,7 @@ func resourceListenerV2Update(d *schema.ResourceData, meta interface{}) error {
 	})
 
 	if err != nil {
-		return fmt.Errorf("Error updating listener %s: %s", d.Id(), err)
+		return fmt.Errorf("Error updating openstack_lb_listener_v2 %s: %s", d.Id(), err)
 	}
 
 	// Wait for the listener to become ACTIVE.
@@ -466,12 +243,12 @@ func resourceListenerV2Delete(d *schema.ResourceData, meta interface{}) error {
 	// Get a clean copy of the listener.
 	listener, err := neutronlisteners.Get(lbClient, d.Id()).Extract()
 	if err != nil {
-		return CheckDeleted(d, err, "Unable to retrieve listener")
+		return CheckDeleted(d, err, "Unable to retrieve openstack_lb_listener_v2")
 	}
 
 	timeout := d.Timeout(schema.TimeoutDelete)
 
-	log.Printf("[DEBUG] Deleting listener %s", d.Id())
+	log.Printf("[DEBUG] Deleting openstack_lb_listener_v2 %s", d.Id())
 	err = resource.Retry(timeout, func() *resource.RetryError {
 		err = neutronlisteners.Delete(lbClient, d.Id()).ExtractErr()
 		if err != nil {
@@ -481,7 +258,7 @@ func resourceListenerV2Delete(d *schema.ResourceData, meta interface{}) error {
 	})
 
 	if err != nil {
-		return CheckDeleted(d, err, "Error deleting listener")
+		return CheckDeleted(d, err, "Error deleting openstack_lb_listener_v2")
 	}
 
 	// Wait for the listener to become DELETED.
