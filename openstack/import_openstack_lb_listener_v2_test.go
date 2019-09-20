da2ec8fc1dd1855976a962c09d82a6f743916695
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-09-20 14:02:38 +0300

    LoadBalancer listener V2: add Octavia import test
    Add TestAccLBV2Listener_importOctavia().

--- openstack/import_openstack_lb_listener_v2_test.go
+++ openstack/import_openstack_lb_listener_v2_test.go
@@ -26,3 +26,24 @@ func TestAccLBV2Listener_importBasic(t *testing.T) {
 		},
 	})
 }
+
+func TestAccLBV2Listener_importOctavia(t *testing.T) {
+	resourceName := "openstack_lb_listener_v2.listener_1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckLB(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLBV2ListenerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: TestAccLBV2ListenerConfig_octavia,
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
