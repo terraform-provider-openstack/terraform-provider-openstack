f45886696a21cce129fab0ac1fdfec68ba658390
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-10-13 17:15:46 +0300

    Networking: cleanup subnet V2
    Cleanup openstack_networking_subnet resource and datasource.
    
    Implement tests for networking_subnet_v2 helpers.
    
    Change "dns_nameservers" argument form list to set to remove unnecessary
    helper that verified if nameservers are distinct.

--- openstack/networking_subnet_v2.go
+++ openstack/networking_subnet_v2.go
@@ -1,5 +1,108 @@
 package openstack
 
+import (
+	"log"
+
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/subnets"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func networkingSubnetV2StateRefreshFunc(client *gophercloud.ServiceClient, subnetID string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		subnet, err := subnets.Get(client, subnetID).Extract()
+		if err != nil {
+			if _, ok := err.(gophercloud.ErrDefault404); ok {
+				return subnet, "DELETED", nil
+			}
+
+			return nil, "", err
+		}
+
+		return subnet, "ACTIVE", nil
+	}
+}
+
+// networkingSubnetV2GetRawAllocationPoolsValueToExpand selects the resource argument to populate
+// the allocations pool value.
+func networkingSubnetV2GetRawAllocationPoolsValueToExpand(d *schema.ResourceData) []interface{} {
+	// First check allocation_pool since that is the new argument.
+	result := d.Get("allocation_pool").(*schema.Set).List()
+
+	if len(result) == 0 {
+		// If no allocation_pool was specified, check allocation_pools
+		// which is the older legacy argument.
+		result = d.Get("allocation_pools").([]interface{})
+	}
+
+	return result
+}
+
+// expandNetworkingSubnetV2AllocationPools returns a slice of subnets.AllocationPool structs.
+func expandNetworkingSubnetV2AllocationPools(allocationPools []interface{}) []subnets.AllocationPool {
+	result := make([]subnets.AllocationPool, len(allocationPools))
+	for i, raw := range allocationPools {
+		rawMap := raw.(map[string]interface{})
+
+		result[i] = subnets.AllocationPool{
+			Start: rawMap["start"].(string),
+			End:   rawMap["end"].(string),
+		}
+	}
+
+	return result
+}
+
+// flattenNetworkingSubnetV2AllocationPools allows to flatten slice of subnets.AllocationPool structs into
+// a slice of maps.
+func flattenNetworkingSubnetV2AllocationPools(allocationPools []subnets.AllocationPool) []map[string]interface{} {
+	result := make([]map[string]interface{}, len(allocationPools))
+	for i, allocationPool := range allocationPools {
+		pool := make(map[string]interface{})
+		pool["start"] = allocationPool.Start
+		pool["end"] = allocationPool.End
+
+		result[i] = pool
+	}
+
+	return result
+}
+
+// expandNetworkingSubnetV2HostRoutes returns a slice of HostRoute structures.
+func expandNetworkingSubnetV2HostRoutes(rawHostRoutes []interface{}) []subnets.HostRoute {
+	result := make([]subnets.HostRoute, len(rawHostRoutes))
+	for i, raw := range rawHostRoutes {
+		rawMap := raw.(map[string]interface{})
+
+		result[i] = subnets.HostRoute{
+			DestinationCIDR: rawMap["destination_cidr"].(string),
+			NextHop:         rawMap["next_hop"].(string),
+		}
+	}
+
+	return result
+}
+
+func networkingSubnetV2AllocationPoolsCustomizeDiff(diff *schema.ResourceDiff) error {
+	if diff.Id() != "" && diff.HasChange("allocation_pools") {
+		o, n := diff.GetChange("allocation_pools")
+		oldPools := o.([]interface{})
+		newPools := n.([]interface{})
+
+		samePools := networkingSubnetV2AllocationPoolsMatch(oldPools, newPools)
+
+		if samePools {
+			log.Printf("[DEBUG] allocation_pools have not changed. clearing diff")
+			return diff.Clear("allocation_pools")
+		}
+
+	}
+
+	return nil
+}
+
 func networkingSubnetV2AllocationPoolsMatch(oldPools, newPools []interface{}) bool {
 	if len(oldPools) != len(newPools) {
 		return false
--- openstack/networking_subnet_v2_test.go
+++ openstack/networking_subnet_v2_test.go
@@ -3,8 +3,107 @@ package openstack
 import (
 	"testing"
 
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/subnets"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+
 	"github.com/stretchr/testify/assert"
 )
+
+func TestNetworkingSubnetV2AllocationPools(t *testing.T) {
+	allocationPools := []subnets.AllocationPool{
+		{
+			Start: "192.168.0.2",
+			End:   "192.168.0.254",
+		},
+		{
+			Start: "10.0.0.2",
+			End:   "10.255.255.254",
+		},
+	}
+
+	expected := []map[string]interface{}{
+		{
+			"start": "192.168.0.2",
+			"end":   "192.168.0.254",
+		},
+		{
+			"start": "10.0.0.2",
+			"end":   "10.255.255.254",
+		},
+	}
+
+	actual := flattenNetworkingSubnetV2AllocationPools(allocationPools)
+
+	assert.ElementsMatch(t, expected, actual)
+}
+
+func TestExpandNetworkingSubnetV2AllocationPools(t *testing.T) {
+	r := resourceNetworkingSubnetV2()
+	d := r.TestResourceData()
+	d.SetId("1")
+
+	allocationPools := []map[string]interface{}{
+		{
+			"start": "192.168.0.2",
+			"end":   "192.168.0.254",
+		},
+		{
+			"start": "10.0.0.2",
+			"end":   "10.255.255.254",
+		},
+	}
+
+	d.Set("allocation_pool", allocationPools)
+
+	expected := []subnets.AllocationPool{
+		{
+			Start: "192.168.0.2",
+			End:   "192.168.0.254",
+		},
+		{
+			Start: "10.0.0.2",
+			End:   "10.255.255.254",
+		},
+	}
+
+	actual := expandNetworkingSubnetV2AllocationPools(d.Get("allocation_pool").(*schema.Set).List())
+
+	assert.ElementsMatch(t, expected, actual)
+}
+
+func TestExpandNetworkingSubnetV2HostRoutes(t *testing.T) {
+	r := resourceNetworkingSubnetV2()
+	d := r.TestResourceData()
+	d.SetId("1")
+
+	hostRoutes := []map[string]interface{}{
+		{
+			"destination_cidr": "192.168.0.0/24",
+			"next_hop":         "10.0.0.1",
+		},
+		{
+			"destination_cidr": "10.0.0.0/8",
+			"next_hop":         "192.168.0.1",
+		},
+	}
+
+	d.Set("host_routes", hostRoutes)
+
+	expected := []subnets.HostRoute{
+		{
+			DestinationCIDR: "192.168.0.0/24",
+			NextHop:         "10.0.0.1",
+		},
+		{
+			DestinationCIDR: "10.0.0.0/8",
+			NextHop:         "192.168.0.1",
+		},
+	}
+
+	actual := expandNetworkingSubnetV2HostRoutes(d.Get("host_routes").([]interface{}))
+
+	assert.ElementsMatch(t, expected, actual)
+}
 
 func TestNetworkingSubnetV2AllocationPoolsMatch(t *testing.T) {
 	oldPools := []interface{}{
@@ -104,5 +203,4 @@ func TestNetworkingSubnetV2AllocationPoolsMatch(t *testing.T) {
 
 	same = networkingSubnetV2AllocationPoolsMatch(oldPools, newPools)
 	assert.Equal(t, same, false)
-
 }
