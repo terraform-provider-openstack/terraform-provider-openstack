5c9a9b1d0d0867fddcad2135af5010291ce3e095
Dhia Ayachi <dhia.ayachi@bell.ca>
2020-03-20 10:02:24 -0400

    Do not delete the compute resource state if the instance have no network

--- openstack/resource_openstack_compute_instance_v2.go
+++ openstack/resource_openstack_compute_instance_v2.go
@@ -926,21 +926,22 @@ func resourceComputeInstanceV2Delete(d *schema.ResourceData, meta interface{}) e
 	if detachPortBeforeDestroy {
 		allInstanceNetworks, err := getAllInstanceNetworks(d, meta)
 		if err != nil {
-			return CheckDeleted(d, err, "Unable to get openstack_compute_instance_v2 ports")
-		}
+			log.Printf("[WARN] Unable to get openstack_compute_instance_v2 ports: %s", err)
+		} else {
 
-		for _, network := range allInstanceNetworks {
-			if network.Port != "" {
-				stateConf := &resource.StateChangeConf{
-					Pending:    []string{""},
-					Target:     []string{"DETACHED"},
-					Refresh:    computeInterfaceAttachV2DetachFunc(computeClient, d.Id(), network.Port),
-					Timeout:    d.Timeout(schema.TimeoutDelete),
-					Delay:      5 * time.Second,
-					MinTimeout: 5 * time.Second,
-				}
-				if _, err = stateConf.WaitForState(); err != nil {
-					return fmt.Errorf("Error detaching openstack_compute_instance_v2 %s: %s", d.Id(), err)
+			for _, network := range allInstanceNetworks {
+				if network.Port != "" {
+					stateConf := &resource.StateChangeConf{
+						Pending:    []string{""},
+						Target:     []string{"DETACHED"},
+						Refresh:    computeInterfaceAttachV2DetachFunc(computeClient, d.Id(), network.Port),
+						Timeout:    d.Timeout(schema.TimeoutDelete),
+						Delay:      5 * time.Second,
+						MinTimeout: 5 * time.Second,
+					}
+					if _, err = stateConf.WaitForState(); err != nil {
+						return fmt.Errorf("Error detaching openstack_compute_instance_v2 %s: %s", d.Id(), err)
+					}
 				}
 			}
 		}
