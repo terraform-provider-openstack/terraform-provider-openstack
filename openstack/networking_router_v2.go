ab4da2234f3e622b3ca724d0dfea56426b8633ad
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-05-02 18:35:34 +0300

    Remove old in-tree SDK from router helpers
    Fix openstack/networking_router_v2.go imports and run go mod tidy, go
    mod vendor.

--- openstack/networking_router_v2.go
+++ openstack/networking_router_v2.go
@@ -3,7 +3,7 @@ package openstack
 import (
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/layer3/routers"
-	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
 func resourceNetworkingRouterV2StateRefreshFunc(client *gophercloud.ServiceClient, routerID string) resource.StateRefreshFunc {
