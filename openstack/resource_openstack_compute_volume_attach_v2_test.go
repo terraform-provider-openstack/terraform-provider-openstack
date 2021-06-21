dc31bb3359187d93225ee7192d84ef16b5272c5b
nikParasyr <nik.parasyr@protonmail.com>
2021-06-21 16:34:58 +0200

    compute_instance: Use BlockStorageV3 for boot from volume when V2 unavailable
    BlockStorageV2 has been dropped since Xena. Try and use BlockStorageV3
    for bootingfromvolume when V2 returns error

--- openstack/resource_openstack_compute_volume_attach_v2_test.go
+++ openstack/resource_openstack_compute_volume_attach_v2_test.go
@@ -196,7 +196,7 @@ resource "openstack_compute_volume_attach_v2" "va_1" {
 
 func testAccComputeV2VolumeAttachIgnoreVolumeConfirmation() string {
 	return fmt.Sprintf(`
-resource "openstack_blockstorage_volume_v2" "volume_1" {
+resource "openstack_blockstorage_volume_v3" "volume_1" {
   name = "volume_1"
   size = 1
 }
@@ -211,7 +211,7 @@ resource "openstack_compute_instance_v2" "instance_1" {
 
 resource "openstack_compute_volume_attach_v2" "va_1" {
   instance_id = "${openstack_compute_instance_v2.instance_1.id}"
-  volume_id = "${openstack_blockstorage_volume_v2.volume_1.id}"
+  volume_id = "${openstack_blockstorage_volume_v3.volume_1.id}"
   vendor_options {
     ignore_volume_confirmation = true
   }
