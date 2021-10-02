7f0928110b2a5a4ce876551aac0284e74301f9d1
Andrei Ozerov <andrei.ozerov92@gmail.com>
2021-10-02 18:03:12 +0300

    Bump golangci-lint version to v1.42.1 (#1296)
    Update version in CI job.
    
    Add local prefixes in config, remove deprecated linters, comment all
    linters that are currently failing or not tested yet.

--- openstack/resource_openstack_lb_quota_v2.go
+++ openstack/resource_openstack_lb_quota_v2.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/gophercloud/gophercloud/openstack/loadbalancer/v2/quotas"
