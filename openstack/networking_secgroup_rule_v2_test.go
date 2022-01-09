d653f0589507b4918f33860e72ed93247fdd0950
Vyacheslav Vershinin <vershinin.vyacheslav@gmail.com>
2022-01-09 17:23:33 +0200

    Update networking_secgroup_rule_v2_test.go

--- openstack/networking_secgroup_rule_v2_test.go
+++ openstack/networking_secgroup_rule_v2_test.go
@@ -81,7 +81,7 @@ func TestResourceNetworkingSecGroupRuleV2ProtocolString(t *testing.T) {
 		string(rules.ProtocolUDP):       rules.ProtocolUDP,
 		string(rules.ProtocolUDPLite):   rules.ProtocolUDPLite,
 		string(rules.ProtocolVRRP):      rules.ProtocolVRRP,
-		string(rules.ProtocolANY):       rules.ProtocolANY,
+		string(rules.ProtocolAny):       rules.ProtocolAny,
 	}
 
 	for name, expected := range protocols {
