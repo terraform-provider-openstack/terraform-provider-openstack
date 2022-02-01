dff2e7c952a8c639aa3248cb7b6c6d1498b3b6ac
Samuel Mutel <12967891+smutel@users.noreply.github.com>
2022-02-01 16:59:30 +0100

    feat: Be able to specify user_id when creating keypair

--- openstack/compute_keypair_v2.go
+++ openstack/compute_keypair_v2.go
@@ -2,6 +2,10 @@ package openstack
 
 import "github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/keypairs"
 
+const (
+	computeKeyPairV2UserIDMicroversion = "2.10"
+)
+
 // ComputeKeyPairV2CreateOpts is a custom KeyPair struct to include the ValueSpecs field.
 type ComputeKeyPairV2CreateOpts struct {
 	keypairs.CreateOpts
