be4478613c73cecc463e6c9ccf8dc1e7a276fa56
Till! <till@users.noreply.github.com>
2022-02-06 17:37:09 +0100

    Update: enable OS_DEBUG (#1246)
    * Update: enable debug logging in the provider
    
     - when TF_LOG is debug/trace (and OS_DEBUG is not set)
     - enable this by default (person probably wants it)
    
    Resolves: terraform-provider-openstack/terraform-provider-openstack#1203
    
    * Chore: update github.com/gophercloud/utils

--- openstack/provider.go
+++ openstack/provider.go
@@ -2,8 +2,10 @@ package openstack
 
 import (
 	"context"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/meta"
 
@@ -244,6 +246,13 @@ func Provider() *schema.Provider {
 				Optional:    true,
 				Default:     false,
 				Description: descriptions["disable_no_cache_header"],
+			},
+
+			"enable_logging": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: descriptions["enable_logging"],
 			},
 		},
 
@@ -487,10 +496,22 @@ func init() {
 			"automatically, if the initial auth token get expired. Defaults to `true`",
 
 		"max_retries": "How many times HTTP connection should be retried until giving up.",
+
+		"enable_logging": "Outputs very verbose logs with all calls made to and responses from OpenStack",
 	}
 }
 
 func configureProvider(d *schema.ResourceData, terraformVersion string) (interface{}, diag.Diagnostics) {
+	enableLogging := d.Get("enable_logging").(bool)
+	if !enableLogging {
+		// enforce logging (similar to OS_DEBUG) when TF_LOG is 'DEBUG' or 'TRACE'
+		if logLevel := logging.LogLevel(); logLevel != "" && os.Getenv("OS_DEBUG") == "" {
+			if logLevel == "DEBUG" || logLevel == "TRACE" {
+				enableLogging = true
+			}
+		}
+	}
+
 	config := Config{
 		auth.Config{
 			CACertFile:                  d.Get("cacert_file").(string),
@@ -526,6 +547,7 @@ func configureProvider(d *schema.ResourceData, terraformVersion string) (interfa
 			TerraformVersion:            terraformVersion,
 			SDKVersion:                  meta.SDKVersionString(),
 			MutexKV:                     mutexkv.NewMutexKV(),
+			EnableLogger:                enableLogging,
 		},
 	}
 
