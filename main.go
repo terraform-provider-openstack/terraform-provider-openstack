2c092af3ddd40ee8971a43097833a10672a1e7fb
Thomas Dreßen <thomas.dressen@plusserver.com>
2022-07-26 11:41:37 +0200

    Fixed newline linting issue

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 const providerAddr = "registry.terraform.io/terraform-provider-openstack/openstack"
 
 func main() {
-
 	// added debugMode to enable debugging for provider per https://www.terraform.io/plugin/sdkv2/debugging
 	var debugMode bool
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
