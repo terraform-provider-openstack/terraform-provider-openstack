06b1e2daffcfce1f8a9b62fc5b12c2bd2e2150a2
Paweł Kubica <pawel.kubica@comarch.pl>
2022-12-12 19:19:08 +0100

    Update containerinfra_nodegroup tests.
    resource_openstack_containerinfra_nodegroup_v1_test
    - check all attributtes of containerinfra_nodegroup resource
    - add 2 tests: merging and overriding nodegroup's labels
    
    import_openstack_containerinfra_nodegroup_v1_test
    - add 2 tests: merging and overriding nodegroup's labels
    
    data_source_openstack_containerinfra_nodegroup_v1_test
    - fix wrong reference in resourceName
    - check all attributtes of containerinfra_cluster resource

--- openstack/import_openstack_containerinfra_nodegroup_v1_test.go
+++ openstack/import_openstack_containerinfra_nodegroup_v1_test.go
@@ -24,13 +24,70 @@ func TestAccContainerInfraV1NodeGroupImport_basic(t *testing.T) {
 		CheckDestroy:      testAccCheckContainerInfraV1NodeGroupDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccContainerInfraV1NodeGroupUpdate(keypairName, clusterTemplateName, clusterName, nodeGroupName, 1),
+				Config: testAccContainerInfraV1NodeGroupBasic(keypairName, clusterTemplateName, clusterName, nodeGroupName, 1),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccContainerInfraV1NodeGroupImport_mergeLabels(t *testing.T) {
+	resourceName := "openstack_containerinfra_nodegroup_v1.nodegroup_1"
+	clusterName := acctest.RandomWithPrefix("tf-acc-cluster")
+	keypairName := acctest.RandomWithPrefix("tf-acc-keypair")
+	clusterTemplateName := acctest.RandomWithPrefix("tf-acc-clustertemplate")
+	nodeGroupName := acctest.RandomWithPrefix("tf-acc-cluster")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckNonAdminOnly(t)
+			testAccPreCheckContainerInfra(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckContainerInfraV1NodeGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContainerInfraV1NodeGroupMergeLabels(keypairName, clusterTemplateName, clusterName, nodeGroupName, 1),
 			},
 			{
 				ResourceName:            resourceName,
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"cluster_id"},
+				ImportStateVerifyIgnore: []string{"merge_labels", "labels"},
+			},
+		},
+	})
+}
+
+func TestAccContainerInfraV1NodeGroupImport_overrideLabels(t *testing.T) {
+	resourceName := "openstack_containerinfra_nodegroup_v1.nodegroup_1"
+	clusterName := acctest.RandomWithPrefix("tf-acc-cluster")
+	keypairName := acctest.RandomWithPrefix("tf-acc-keypair")
+	clusterTemplateName := acctest.RandomWithPrefix("tf-acc-clustertemplate")
+	nodeGroupName := acctest.RandomWithPrefix("tf-acc-cluster")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckNonAdminOnly(t)
+			testAccPreCheckContainerInfra(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckContainerInfraV1NodeGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContainerInfraV1NodeGroupOverrideLabels(keypairName, clusterTemplateName, clusterName, nodeGroupName, 1),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"merge_labels", "labels"},
 			},
 		},
 	})
--- openstack/resource_openstack_containerinfra_nodegroup_v1_test.go
+++ openstack/resource_openstack_containerinfra_nodegroup_v1_test.go
@@ -31,19 +31,177 @@ func TestAccContainerInfraV1NodeGroup_basic(t *testing.T) {
 		CheckDestroy:      testAccCheckContainerInfraV1NodeGroupDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccContainerInfraV1NodeGroupUpdate(keypairName, clusterTemplateName, clusterName, nodeGroupName, 1),
+				Config: testAccContainerInfraV1NodeGroupBasic(keypairName, clusterTemplateName, clusterName, nodeGroupName, 1),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckContainerInfraV1NodeGroupExists(resourceName, &nodeGroup),
+					resource.TestCheckResourceAttr(resourceName, "region", osRegionName),
+					resource.TestCheckResourceAttrSet(resourceName, "cluster_id"),
 					resource.TestCheckResourceAttr(resourceName, "name", nodeGroupName),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "updated_at"),
+					resource.TestCheckResourceAttr(resourceName, "docker_volume_size", strconv.Itoa(10)),
+					resource.TestCheckResourceAttrSet(resourceName, "labels.%"),
+					resource.TestCheckResourceAttr(resourceName, "labels.kubescheduler_options", "log-flush-frequency=1m"),
+					resource.TestCheckResourceAttr(resourceName, "role", "myRole"),
 					resource.TestCheckResourceAttr(resourceName, "node_count", strconv.Itoa(1)),
+					resource.TestCheckResourceAttr(resourceName, "min_node_count", strconv.Itoa(1)),
+					resource.TestCheckResourceAttr(resourceName, "max_node_count", strconv.Itoa(5)),
+					resource.TestCheckResourceAttr(resourceName, "image_id", osMagnumImage),
+					resource.TestCheckResourceAttr(resourceName, "flavor_id", osMagnumFlavor),
 				),
 			},
 			{
-				Config: testAccContainerInfraV1NodeGroupUpdate(keypairName, clusterTemplateName, clusterName, nodeGroupName, 2),
+				Config: testAccContainerInfraV1NodeGroupBasic(keypairName, clusterTemplateName, clusterName, nodeGroupName, 2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckContainerInfraV1NodeGroupExists(resourceName, &nodeGroup),
+					resource.TestCheckResourceAttr(resourceName, "region", osRegionName),
+					resource.TestCheckResourceAttrSet(resourceName, "cluster_id"),
 					resource.TestCheckResourceAttr(resourceName, "name", nodeGroupName),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "updated_at"),
+					resource.TestCheckResourceAttr(resourceName, "docker_volume_size", strconv.Itoa(10)),
+					resource.TestCheckResourceAttrSet(resourceName, "labels.%"),
+					resource.TestCheckResourceAttr(resourceName, "labels.kubescheduler_options", "log-flush-frequency=1m"),
+					resource.TestCheckResourceAttr(resourceName, "role", "myRole"),
 					resource.TestCheckResourceAttr(resourceName, "node_count", strconv.Itoa(2)),
+					resource.TestCheckResourceAttr(resourceName, "min_node_count", strconv.Itoa(1)),
+					resource.TestCheckResourceAttr(resourceName, "max_node_count", strconv.Itoa(5)),
+					resource.TestCheckResourceAttr(resourceName, "image_id", osMagnumImage),
+					resource.TestCheckResourceAttr(resourceName, "flavor_id", osMagnumFlavor),
+				),
+			},
+		},
+	})
+}
+
+func TestAccContainerInfraV1NodeGroup_mergeLabels(t *testing.T) {
+	var nodeGroup nodegroups.NodeGroup
+
+	resourceName := "openstack_containerinfra_nodegroup_v1.nodegroup_1"
+	clusterName := acctest.RandomWithPrefix("tf-acc-cluster")
+	keypairName := acctest.RandomWithPrefix("tf-acc-keypair")
+	clusterTemplateName := acctest.RandomWithPrefix("tf-acc-clustertemplate")
+	nodeGroupName := acctest.RandomWithPrefix("tf-acc-nodegroup")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckNonAdminOnly(t)
+			testAccPreCheckContainerInfra(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckContainerInfraV1NodeGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContainerInfraV1NodeGroupMergeLabels(keypairName, clusterTemplateName, clusterName, nodeGroupName, 1),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContainerInfraV1NodeGroupExists(resourceName, &nodeGroup),
+					resource.TestCheckResourceAttr(resourceName, "region", osRegionName),
+					resource.TestCheckResourceAttrSet(resourceName, "cluster_id"),
+					resource.TestCheckResourceAttr(resourceName, "name", nodeGroupName),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "updated_at"),
+					resource.TestCheckResourceAttr(resourceName, "docker_volume_size", strconv.Itoa(10)),
+					resource.TestCheckResourceAttrSet(resourceName, "labels.%"),
+					resource.TestCheckResourceAttr(resourceName, "labels.boot_volume_size", "15"),
+					resource.TestCheckResourceAttr(resourceName, "merge_labels", "true"),
+					resource.TestCheckResourceAttr(resourceName, "role", "myRole"),
+					resource.TestCheckResourceAttr(resourceName, "node_count", strconv.Itoa(1)),
+					resource.TestCheckResourceAttr(resourceName, "min_node_count", strconv.Itoa(1)),
+					resource.TestCheckResourceAttr(resourceName, "max_node_count", strconv.Itoa(5)),
+					resource.TestCheckResourceAttr(resourceName, "image_id", osMagnumImage),
+					resource.TestCheckResourceAttr(resourceName, "flavor_id", osMagnumFlavor),
+				),
+			},
+			{
+				Config: testAccContainerInfraV1NodeGroupMergeLabels(keypairName, clusterTemplateName, clusterName, nodeGroupName, 2),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContainerInfraV1NodeGroupExists(resourceName, &nodeGroup),
+					resource.TestCheckResourceAttr(resourceName, "region", osRegionName),
+					resource.TestCheckResourceAttrSet(resourceName, "cluster_id"),
+					resource.TestCheckResourceAttr(resourceName, "name", nodeGroupName),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "updated_at"),
+					resource.TestCheckResourceAttr(resourceName, "docker_volume_size", strconv.Itoa(10)),
+					resource.TestCheckResourceAttrSet(resourceName, "labels.%"),
+					resource.TestCheckResourceAttr(resourceName, "labels.boot_volume_size", "15"),
+					resource.TestCheckResourceAttr(resourceName, "merge_labels", "true"),
+					resource.TestCheckResourceAttr(resourceName, "role", "myRole"),
+					resource.TestCheckResourceAttr(resourceName, "node_count", strconv.Itoa(2)),
+					resource.TestCheckResourceAttr(resourceName, "min_node_count", strconv.Itoa(1)),
+					resource.TestCheckResourceAttr(resourceName, "max_node_count", strconv.Itoa(5)),
+					resource.TestCheckResourceAttr(resourceName, "image_id", osMagnumImage),
+					resource.TestCheckResourceAttr(resourceName, "flavor_id", osMagnumFlavor),
+				),
+			},
+		},
+	})
+}
+
+func TestAccContainerInfraV1NodeGroup_overrideLabels(t *testing.T) {
+	var nodeGroup nodegroups.NodeGroup
+
+	resourceName := "openstack_containerinfra_nodegroup_v1.nodegroup_1"
+	clusterName := acctest.RandomWithPrefix("tf-acc-cluster")
+	keypairName := acctest.RandomWithPrefix("tf-acc-keypair")
+	clusterTemplateName := acctest.RandomWithPrefix("tf-acc-clustertemplate")
+	nodeGroupName := acctest.RandomWithPrefix("tf-acc-nodegroup")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckNonAdminOnly(t)
+			testAccPreCheckContainerInfra(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckContainerInfraV1NodeGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContainerInfraV1NodeGroupOverrideLabels(keypairName, clusterTemplateName, clusterName, nodeGroupName, 1),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContainerInfraV1NodeGroupExists(resourceName, &nodeGroup),
+					resource.TestCheckResourceAttr(resourceName, "region", osRegionName),
+					resource.TestCheckResourceAttrSet(resourceName, "cluster_id"),
+					resource.TestCheckResourceAttr(resourceName, "name", nodeGroupName),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "updated_at"),
+					resource.TestCheckResourceAttr(resourceName, "docker_volume_size", strconv.Itoa(10)),
+					resource.TestCheckResourceAttrSet(resourceName, "labels.%"),
+					resource.TestCheckResourceAttr(resourceName, "labels.kubescheduler_options", "log-flush-frequency=2m"),
+					resource.TestCheckResourceAttr(resourceName, "merge_labels", "false"),
+					resource.TestCheckResourceAttr(resourceName, "role", "myRole"),
+					resource.TestCheckResourceAttr(resourceName, "node_count", strconv.Itoa(1)),
+					resource.TestCheckResourceAttr(resourceName, "min_node_count", strconv.Itoa(1)),
+					resource.TestCheckResourceAttr(resourceName, "max_node_count", strconv.Itoa(5)),
+					resource.TestCheckResourceAttr(resourceName, "image_id", osMagnumImage),
+					resource.TestCheckResourceAttr(resourceName, "flavor_id", osMagnumFlavor),
+				),
+			},
+			{
+				Config: testAccContainerInfraV1NodeGroupOverrideLabels(keypairName, clusterTemplateName, clusterName, nodeGroupName, 2),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContainerInfraV1NodeGroupExists(resourceName, &nodeGroup),
+					resource.TestCheckResourceAttr(resourceName, "region", osRegionName),
+					resource.TestCheckResourceAttrSet(resourceName, "cluster_id"),
+					resource.TestCheckResourceAttr(resourceName, "name", nodeGroupName),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "updated_at"),
+					resource.TestCheckResourceAttr(resourceName, "docker_volume_size", strconv.Itoa(10)),
+					resource.TestCheckResourceAttrSet(resourceName, "labels.%"),
+					resource.TestCheckResourceAttr(resourceName, "labels.kubescheduler_options", "log-flush-frequency=2m"),
+					resource.TestCheckResourceAttr(resourceName, "merge_labels", "false"),
+					resource.TestCheckResourceAttr(resourceName, "role", "myRole"),
+					resource.TestCheckResourceAttr(resourceName, "node_count", strconv.Itoa(2)),
+					resource.TestCheckResourceAttr(resourceName, "min_node_count", strconv.Itoa(1)),
+					resource.TestCheckResourceAttr(resourceName, "max_node_count", strconv.Itoa(5)),
+					resource.TestCheckResourceAttr(resourceName, "image_id", osMagnumImage),
+					resource.TestCheckResourceAttr(resourceName, "flavor_id", osMagnumFlavor),
 				),
 			},
 		},
@@ -113,7 +271,8 @@ func testAccCheckContainerInfraV1NodeGroupDestroy(s *terraform.State) error {
 
 	return nil
 }
-func testAccContainerInfraV1NodeGroupUpdate(keypairName, clusterTemplateName, clusterName string, nodeGroupName string, nodeCount int) string {
+
+func testAccContainerInfraV1NodeGroupBasic(keypairName, clusterTemplateName, clusterName string, nodeGroupName string, nodeCount int) string {
 	return fmt.Sprintf(`
 resource "openstack_compute_keypair_v2" "keypair_1" {
   name = "%s"
@@ -136,6 +295,7 @@ resource "openstack_containerinfra_clustertemplate_v1" "clustertemplate_1" {
   no_proxy              = "%s"
   labels = {
     kubescheduler_options = "log-flush-frequency=1m",
+	%s
   }
 }
 
@@ -151,6 +311,119 @@ resource "openstack_containerinfra_nodegroup_v1" "nodegroup_1" {
   name                 = "%s"
   cluster_id           = "${openstack_containerinfra_cluster_v1.cluster_1.id}"
   node_count           = %d
+  docker_volume_size   = 10
+  role				   = "myRole"
+  min_node_count       = 1
+  max_node_count       = 5
+  image_id             = "%s"
+  flavor_id            = "%s"
 }
-`, keypairName, clusterTemplateName, osMagnumImage, osMagnumFlavor, osMagnumFlavor, osExtGwID, osMagnumHTTPProxy, osMagnumHTTPSProxy, osMagnumNoProxy, clusterName, nodeGroupName, nodeCount)
+`, keypairName, clusterTemplateName, osMagnumImage, osMagnumFlavor, osMagnumFlavor, osExtGwID, osMagnumHTTPProxy, osMagnumHTTPSProxy, osMagnumNoProxy, osMagnumLabels, clusterName, nodeGroupName, nodeCount, osMagnumImage, osMagnumFlavor)
+}
+
+func testAccContainerInfraV1NodeGroupMergeLabels(keypairName, clusterTemplateName, clusterName string, nodeGroupName string, nodeCount int) string {
+	return fmt.Sprintf(`
+resource "openstack_compute_keypair_v2" "keypair_1" {
+  name = "%s"
+}
+
+resource "openstack_containerinfra_clustertemplate_v1" "clustertemplate_1" {
+  name                  = "%s"
+  image                 = "%s"
+  coe                   = "kubernetes"
+  master_flavor         = "%s"
+  flavor                = "%s"
+  floating_ip_enabled   = true
+  volume_driver         = "cinder"
+  docker_storage_driver = "overlay2"
+  docker_volume_size    = 5
+  external_network_id   = "%s"
+  network_driver        = "flannel"
+  http_proxy            = "%s"
+  https_proxy           = "%s"
+  no_proxy              = "%s"
+  labels = {
+    kubescheduler_options = "log-flush-frequency=1m",
+	%s
+  }
+}
+
+resource "openstack_containerinfra_cluster_v1" "cluster_1" {
+  name                 = "%s"
+  cluster_template_id  = "${openstack_containerinfra_clustertemplate_v1.clustertemplate_1.id}"
+  master_count         = 1
+  node_count           = 1
+  keypair              = "${openstack_compute_keypair_v2.keypair_1.name}"
+}
+
+resource "openstack_containerinfra_nodegroup_v1" "nodegroup_1" {
+  name                 = "%s"
+  cluster_id           = "${openstack_containerinfra_cluster_v1.cluster_1.id}"
+  node_count           = %d
+  docker_volume_size   = 10
+  role				   = "myRole"
+  min_node_count       = 1
+  max_node_count       = 5
+  image_id             = "%s"
+  flavor_id            = "%s"
+  merge_labels         = true
+  labels = {
+    boot_volume_size = 15
+  }
+}
+`, keypairName, clusterTemplateName, osMagnumImage, osMagnumFlavor, osMagnumFlavor, osExtGwID, osMagnumHTTPProxy, osMagnumHTTPSProxy, osMagnumNoProxy, osMagnumLabels, clusterName, nodeGroupName, nodeCount, osMagnumImage, osMagnumFlavor)
+}
+
+func testAccContainerInfraV1NodeGroupOverrideLabels(keypairName, clusterTemplateName, clusterName string, nodeGroupName string, nodeCount int) string {
+	return fmt.Sprintf(`
+resource "openstack_compute_keypair_v2" "keypair_1" {
+  name = "%s"
+}
+
+resource "openstack_containerinfra_clustertemplate_v1" "clustertemplate_1" {
+  name                  = "%s"
+  image                 = "%s"
+  coe                   = "kubernetes"
+  master_flavor         = "%s"
+  flavor                = "%s"
+  floating_ip_enabled   = true
+  volume_driver         = "cinder"
+  docker_storage_driver = "overlay2"
+  docker_volume_size    = 5
+  external_network_id   = "%s"
+  network_driver        = "flannel"
+  http_proxy            = "%s"
+  https_proxy           = "%s"
+  no_proxy              = "%s"
+  labels = {
+    kubescheduler_options = "log-flush-frequency=1m",
+	%s
+  }
+}
+
+resource "openstack_containerinfra_cluster_v1" "cluster_1" {
+  name                 = "%s"
+  cluster_template_id  = "${openstack_containerinfra_clustertemplate_v1.clustertemplate_1.id}"
+  master_count         = 1
+  node_count           = 1
+  keypair              = "${openstack_compute_keypair_v2.keypair_1.name}"
+}
+
+resource "openstack_containerinfra_nodegroup_v1" "nodegroup_1" {
+  name                 = "%s"
+  cluster_id           = "${openstack_containerinfra_cluster_v1.cluster_1.id}"
+  node_count           = %d
+  docker_volume_size   = 10
+  role				   = "myRole"
+  min_node_count       = 1
+  max_node_count       = 5
+  image_id             = "%s"
+  flavor_id            = "%s"
+  merge_labels         = false
+  labels = {
+	kubescheduler_options = "log-flush-frequency=2m",
+	%s
+  }
+}
+`, keypairName, clusterTemplateName, osMagnumImage, osMagnumFlavor, osMagnumFlavor, osExtGwID, osMagnumHTTPProxy, osMagnumHTTPSProxy, osMagnumNoProxy, osMagnumLabels, clusterName, nodeGroupName, nodeCount, osMagnumImage, osMagnumFlavor, osMagnumLabels)
 }
