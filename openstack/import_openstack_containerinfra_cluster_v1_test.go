e59d69b0dcd6645ff993203caae3f6a27faff9d7
Paweł Kubica <pawel.kubica@comarch.pl>
2022-12-12 17:21:40 +0100

    Update containerinfra_cluster tests.
    resource_openstack_containerinfra_cluster_v1
    - set missing values of resoure's attributes
    
    resource_openstack_containerinfra_cluster_v1_test
    - check all attributtes of containerinfra_cluster resource
    - add 2 tests: merging and overriding cluster's labels
    
    import_openstack_containerinfra_cluster_v1_test
    - add 2 tests: merging and overriding cluster's labels
    
    data_source_openstack_containerinfra_cluster_v1_test
    - fix wrong reference in resourceName
    - check all attributtes of containerinfra_cluster resource

--- openstack/import_openstack_containerinfra_cluster_v1_test.go
+++ openstack/import_openstack_containerinfra_cluster_v1_test.go
@@ -23,13 +23,69 @@ func TestAccContainerInfraV1ClusterImport_basic(t *testing.T) {
 		CheckDestroy:      testAccCheckContainerInfraV1ClusterDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccContainerInfraV1ClusterBasic(keypairName, clusterTemplateName, clusterName),
+				Config: testAccContainerInfraV1ClusterBasic(keypairName, clusterTemplateName, clusterName, 1),
 			},
 			{
 				ResourceName:            resourceName,
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"keypair", "cluster_template_id"},
+				ImportStateVerifyIgnore: []string{"kubeconfig"},
+			},
+		},
+	})
+}
+
+func TestAccContainerInfraV1ClusterImport_mergeLabels(t *testing.T) {
+	resourceName := "openstack_containerinfra_cluster_v1.cluster_1"
+	clusterName := acctest.RandomWithPrefix("tf-acc-cluster")
+	keypairName := acctest.RandomWithPrefix("tf-acc-keypair")
+	clusterTemplateName := acctest.RandomWithPrefix("tf-acc-clustertemplate")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckNonAdminOnly(t)
+			testAccPreCheckContainerInfra(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckContainerInfraV1ClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContainerInfraV1ClusterLabels(keypairName, clusterTemplateName, clusterName, 1, true),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"kubeconfig", "merge_labels", "labels"},
+			},
+		},
+	})
+}
+
+func TestAccContainerInfraV1ClusterImport_overrideLabels(t *testing.T) {
+	resourceName := "openstack_containerinfra_cluster_v1.cluster_1"
+	clusterName := acctest.RandomWithPrefix("tf-acc-cluster")
+	keypairName := acctest.RandomWithPrefix("tf-acc-keypair")
+	clusterTemplateName := acctest.RandomWithPrefix("tf-acc-clustertemplate")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckNonAdminOnly(t)
+			testAccPreCheckContainerInfra(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckContainerInfraV1ClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContainerInfraV1ClusterLabels(keypairName, clusterTemplateName, clusterName, 1, false),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"kubeconfig", "merge_labels", "labels"},
 			},
 		},
 	})
