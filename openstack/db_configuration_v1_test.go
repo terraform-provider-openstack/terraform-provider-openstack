ccaf8704daf008d75a7e16b683e1092252550282
Jonathan Herlin <jonte@jherlin.se>
2023-03-23 15:53:39 +0100

    Expand Trove test

--- openstack/db_configuration_v1_test.go
+++ openstack/db_configuration_v1_test.go
@@ -39,12 +39,35 @@ func TestExpandDatabaseConfigurationV1Values(t *testing.T) {
 			"name":  "max_connections",
 			"value": "200",
 		},
+		map[string]interface{}{
+			"name":        "collation_connection",
+			"value":       "47",
+			"string_type": false,
+		},
+		map[string]interface{}{
+			"name":        "connect_timeout",
+			"value":       "3",
+			"string_type": true,
+		},
+		map[string]interface{}{
+			"name":  "autocommit",
+			"value": "true",
+		},
+		map[string]interface{}{
+			"name":        "sync_binlog",
+			"value":       "true",
+			"string_type": true,
+		},
 	}
 
 	expected := map[string]interface{}{
-		"collation_server":   "latin1_swedish_ci",
-		"collation_database": "latin1_swedish_ci",
-		"max_connections":    200,
+		"collation_server":     "latin1_swedish_ci",
+		"collation_database":   "latin1_swedish_ci",
+		"max_connections":      200,
+		"collation_connection": 47,
+		"connect_timeout":      "3",
+		"autocommit":           true,
+		"sync_binlog":          "true",
 	}
 
 	actual := expandDatabaseConfigurationV1Values(values)
