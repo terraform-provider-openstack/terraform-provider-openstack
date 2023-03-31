e3dfcee974207f9495c1825d891032e89ff3a658
kayrus <kay.diam@gmail.com>
2023-03-31 10:17:50 +0200

    Remove deprecated calls

--- openstack/provider_test.go
+++ openstack/provider_test.go
@@ -3,7 +3,6 @@ package openstack
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"testing"
 
@@ -417,7 +416,7 @@ func envVarFile(varName string) (string, error) {
 		return "", err
 	}
 
-	tmpFile, err := ioutil.TempFile("", varName)
+	tmpFile, err := os.CreateTemp("", varName)
 	if err != nil {
 		return "", fmt.Errorf("Error creating temp file: %s", err)
 	}
