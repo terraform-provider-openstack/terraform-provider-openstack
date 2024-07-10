781e25666f0bfdec94ae444e53f18198ad346889
kayrus <kay.diam@gmail.com>
2024-07-10 13:27:12 +0200

    swift: add openstack_objectstorage_account_v1 resource

---
+++ docs/resources/objectstorage_account_v1.md
@@ -0,0 +1,61 @@
+---
+subcategory: "Object Storage / Swift"
+layout: "openstack"
+page_title: "OpenStack: openstack_objectstorage_account_v1"
+sidebar_current: "docs-openstack-resource-objectstorage-account-v1"
+description: |-
+  Manages a V1 account resource within OpenStack.
+---
+
+# openstack\_objectstorage\_account\_v1
+
+Manages a V1 account resource within OpenStack.
+
+## Example Usage
+
+```hcl
+resource "openstack_objectstorage_account_v1" "account_1" {
+  region = "RegionOne"
+
+  metadata = {
+    Temp-Url-Key = "testkey"
+    test         = "true"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional) The region in which to create the account. If omitted,
+  the `region` argument of the provider is used. Changing this creates a new
+  account.
+
+* `project_id` - (Optional) The project ID of the corresponding account. If
+  omitted, the token's project ID is used. Changing this creates a new account.
+
+* `metadata` - (Optional) A map of custom key/value pairs to associate with the
+  account metadata. Changing the `Quota-Bytes` key value is allowed to be
+  updated only by the cloud administrator.
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `region` - See Argument Reference above.
+* `project_id` - See Argument Reference above.
+* `metadata` - See Argument Reference above.
+* `headers` - A map of headers returned for the account.
+* `bytes_used` - The number of bytes used by the account.
+* `quota_bytes` - The number of bytes allowed for the account.
+* `container_count` - The number of containers in the account.
+* `object_count` - The number of objects in the account.
+
+## Import
+
+This resource can be imported by specifying the project ID of the account:
+
+```
+$ terraform import openstack_objectstorage_account_v1.account_1 1202b3d0aaa44cfc8b79475c007b0711
+```
---
+++ openstack/resource_openstack_objectstorage_account_v1_test.go
@@ -0,0 +1,124 @@
+package openstack
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/gophercloud/gophercloud/v2/openstack/objectstorage/v1/accounts"
+)
+
+func TestAccObjectStorageV1Account_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckNonAdminOnly(t)
+			testAccPreCheckSwift(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckObjectStorageV1AccountDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccObjectStorageV1AccountBasic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"openstack_objectstorage_account_v1.account_1", "metadata.%", "2"),
+					resource.TestCheckResourceAttr(
+						"openstack_objectstorage_account_v1.account_1", "metadata.Test", "true"),
+					resource.TestCheckResourceAttr(
+						"openstack_objectstorage_account_v1.account_1", "metadata.Uppertest", "true"),
+				),
+			},
+			{
+				Config: testAccObjectStorageV1AccountUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"openstack_objectstorage_account_v1.account_1", "metadata.%", "1"),
+					resource.TestCheckResourceAttr(
+						"openstack_objectstorage_account_v1.account_1", "metadata.Test", "true"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccObjectStorageV1Account_quota(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckAdminOnly(t)
+			testAccPreCheckSwift(t)
+			t.Skip("Works only in selected environments")
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckObjectStorageV1AccountDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccObjectStorageV1AccountQuota,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"openstack_objectstorage_account_v1.account_1", "metadata.%", "2"),
+					resource.TestCheckResourceAttr(
+						"openstack_objectstorage_account_v1.account_1", "metadata.Test", "true"),
+					resource.TestCheckResourceAttr(
+						"openstack_objectstorage_account_v1.account_1", "metadata.Quota-Bytes", "1024000"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckObjectStorageV1AccountDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+	objectStorageClient, err := config.ObjectStorageV1Client(context.TODO(), osRegionName)
+	if err != nil {
+		return fmt.Errorf("Error creating OpenStack object storage client: %s", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "openstack_objectstorage_account_v1" {
+			continue
+		}
+
+		res := accounts.Get(context.TODO(), objectStorageClient, nil)
+		metadata, err := res.ExtractMetadata()
+		if err != nil {
+			return fmt.Errorf("failed to retrieve account metadata: %s", err)
+		}
+
+		if len(metadata) > 1 {
+			return fmt.Errorf("account metadata still exists")
+		}
+	}
+
+	return nil
+}
+
+const testAccObjectStorageV1AccountBasic = `
+resource "openstack_objectstorage_account_v1" "account_1" {
+  metadata = {
+    test = "true"
+    upperTest = "true"
+  }
+}
+`
+
+const testAccObjectStorageV1AccountUpdate = `
+resource "openstack_objectstorage_account_v1" "account_1" {
+  metadata = {
+    test = "true"
+  }
+}
+`
+
+const testAccObjectStorageV1AccountQuota = `
+resource "openstack_objectstorage_account_v1" "account_1" {
+  metadata = {
+    test = "true"
+    quota-bytes = "1024000"
+  }
+}
+`
