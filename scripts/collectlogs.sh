d01ac9b669413aa81415fb34d6dba6c13ca02300
Anthony ROUSSEL <anthony@roussel.dev>
2024-11-08 23:02:42 +0100

    [GH-A] Collect apache2 logs in case of CI failure

--- scripts/collectlogs.sh
+++ scripts/collectlogs.sh
@@ -7,5 +7,7 @@ sudo journalctl -o short-precise --no-pager &> $LOG_DIR/journal.log
 free -m > $LOG_DIR/free.txt
 dpkg -l > $LOG_DIR/dpkg-l.txt
 pip freeze > $LOG_DIR/pip-freeze.txt
+mkdir -p $LOG_DIR/apache2
+cp /var/log/apache2/* $LOG_DIR/apache2/.
 sudo find $LOG_DIR -type d -exec chmod 0755 {} \;
 sudo find $LOG_DIR -type f -exec chmod 0644 {} \;
