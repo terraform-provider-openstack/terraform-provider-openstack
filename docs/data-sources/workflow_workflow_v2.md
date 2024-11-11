b5ad2be9b6f0ae470d5e3d587cc26624043ea564
Anthony ROUSSEL <anthony@roussel.dev>
2024-11-11 14:16:31 +0100

    [workflow] add openstack_workflow_workflow_v2 datasource

---
+++ docs/data-sources/workflow_workflow_v2.md
@@ -0,0 +1,46 @@
+---
+subcategory: "Workflow / Mistral"
+layout: "openstack"
+page_title: "OpenStack: openstack_workflow_workflow_v2"
+sidebar_current: "docs-openstack-datasource-workflow-workflow-v2"
+description: |-
+  Get information on a workflow.
+---
+
+# openstack\_workflow\_workflow_v2
+
+Use this data source to get the ID of an available workflow.
+
+## Example Usage
+
+```hcl
+data "openstack_workflow_workflow_v2" "workflow_1" {
+  name = "workflow_1"
+}
+```
+
+## Argument Reference
+
+* `region` - (Optional) The region in which to obtain the V2 Workflow client.
+
+* `name` - (Optional) The name of the workflow.
+
+* `namespace` - (Optional) The namespace of the workflow.
+
+* `project_id` - (Optional) The id of the project to retrieve the workflow.
+    Requires admin privileges.
+
+## Attributes Reference
+
+`id` is set to the ID of the found workflow. In addition, the following attributes
+are exported:
+
+* `region` - See Argument Reference above.
+* `name` - See Argument Reference above.
+* `namespace` - See Argument Reference above.
+* `project_id` - See Argument Reference above.
+* `input` - A set of input parameters required for workflow execution.
+* `definition` - The workflow definition in Mistral v2 DSL.
+* `tags` - A set of string tags for the workflow.
+* `scope` - Scope (private or public).
+* `created_at` - The date the workflow was created.
---
+++ openstack/data_source_openstack_workflow_workflow_v2.go
@@ -0,0 +1,140 @@
+package openstack
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/gophercloud/gophercloud/v2/openstack/workflow/v2/workflows"
+)
+
+func dataSourceWorkflowWorkflowV2() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceWorkflowWorkflowV2Read,
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"namespace": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"project_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"input": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"definition": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"tags": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
+
+			"scope": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceWorkflowWorkflowV2Read(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*Config)
+	workflowClient, err := config.WorkflowV2Client(ctx, GetRegion(d, config))
+	if err != nil {
+		return diag.Errorf("Error creating OpenStack workflow client: %s", err)
+	}
+
+	listOpts := workflows.ListOpts{
+		ProjectID: d.Get("project_id").(string),
+	}
+
+	name := d.Get("name").(string)
+	if name != "" {
+		listOpts.Name = &workflows.ListFilter{
+			Filter: "eq",
+			Value:  name,
+		}
+	}
+
+	namespace := d.Get("namespace").(string)
+	if namespace != "" {
+		listOpts.Namespace = &workflows.ListFilter{
+			Filter: "eq",
+			Value:  namespace,
+		}
+	}
+
+	allPages, err := workflows.List(workflowClient, listOpts).AllPages(ctx)
+	if err != nil {
+		return diag.Errorf("Unable to query workflows: %s", err)
+	}
+
+	allWorkflows, err := workflows.ExtractWorkflows(allPages)
+	if err != nil {
+		return diag.Errorf("Unable to retrieve workflows: %s", err)
+	}
+
+	if len(allWorkflows) < 1 {
+		return diag.Errorf("Your query returned no results. Please change your search criteria and try again")
+	}
+
+	var workflow workflows.Workflow
+	if len(allWorkflows) > 1 {
+		log.Printf("[DEBUG] Multiple results found: %#v", allWorkflows)
+		return diag.Errorf("Your query returned more than one result. Please try a more specific search criteria")
+	}
+	workflow = allWorkflows[0]
+
+	dataSourceWorkflowWorkflowV2Attributes(d, &workflow, GetRegion(d, config))
+
+	return nil
+}
+
+func dataSourceWorkflowWorkflowV2Attributes(d *schema.ResourceData, workflow *workflows.Workflow, region string) {
+	d.SetId(workflow.ID)
+	d.Set("region", region)
+	d.Set("name", workflow.Name)
+	d.Set("namespace", workflow.Namespace)
+	d.Set("input", workflow.Input)
+	d.Set("definition", workflow.Definition)
+	d.Set("tags", workflow.Tags)
+	d.Set("scope", workflow.Scope)
+	d.Set("project_id", workflow.ProjectID)
+
+	if err := d.Set("created_at", workflow.CreatedAt.Format(time.RFC3339)); err != nil {
+		log.Printf("[DEBUG] Unable to set created_at for openstack_workflow_workflow_v2 %s: %s", workflow.ID, err)
+	}
+}
