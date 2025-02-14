49e4a47daeae9d69d45984f4dbc86d38f116479e
victorcam <victor.camargo@adyen.com>
2025-02-14 10:06:55 +0100

    Renaming resource from openstack_dns_shared_zone_v2 to openstack_dns_zone_share_v2

--- openstack/data_source_openstack_dns_shared_zone_v2_test.go
+++ openstack/data_source_openstack_dns_shared_zone_v2_test.go
@@ -19,9 +19,9 @@ func TestAccDataSourceDNSZoneShareV2_basic(t *testing.T) {
 			{
 				Config: testAccDataSourceDNSZoneShareV2Config(zoneID, projectID, sudoProjectID),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.openstack_dns_shared_zone_v2.test", "zone_id", zoneID),
-					resource.TestCheckResourceAttr("data.openstack_dns_shared_zone_v2.test", "target_project_id", projectID),
-					resource.TestCheckResourceAttr("data.openstack_dns_shared_zone_v2.test", "project_id", sudoProjectID),
+					resource.TestCheckResourceAttr("data.openstack_dns_zone_share_v2.test", "zone_id", zoneID),
+					resource.TestCheckResourceAttr("data.openstack_dns_zone_share_v2.test", "target_project_id", projectID),
+					resource.TestCheckResourceAttr("data.openstack_dns_zone_share_v2.test", "project_id", sudoProjectID),
 				),
 			},
 		},
@@ -30,7 +30,7 @@ func TestAccDataSourceDNSZoneShareV2_basic(t *testing.T) {
 
 func testAccDataSourceDNSZoneShareV2Config(zoneID, projectID, sudoProjectID string) string {
 	return fmt.Sprintf(`
-data "openstack_dns_shared_zone_v2" "test" {
+data "openstack_dns_zone_share_v2" "test" {
   zone_id            = "%s"
   target_project_id  = "%s"
   project_id         = "%s"
