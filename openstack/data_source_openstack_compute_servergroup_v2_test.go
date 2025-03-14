e4e4383dbae348fdc8dac8a7d2a10ee6bde764db
Askar Sabyrov <askar.sabyrov@scity.pro>
2025-03-14 16:46:00 +0500

    try to fix acceptance test

--- openstack/data_source_openstack_compute_servergroup_v2_test.go
+++ openstack/data_source_openstack_compute_servergroup_v2_test.go
@@ -2,7 +2,6 @@ package openstack
 
 import (
 	"fmt"
-	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -23,7 +22,7 @@ func TestAccComputeServerGroupV2DataSource(t *testing.T) {
 					testAccCheckComputeServerGroupV2DataSourceID("data.openstack_compute_servergroup_v2.server_group_1"),
 					resource.TestCheckResourceAttr("data.openstack_compute_servergroup_v2.server_group_1", "name", "my-servergroup"),
 					resource.TestCheckResourceAttr("data.openstack_compute_servergroup_v2.server_group_1", "policy", "anti-affinity"),
-					resource.TestCheckResourceAttr("data.openstack_compute_servergroup_v2.server_group_1", "rules.max_server_per_host", strconv.Itoa(3)),
+					resource.TestCheckResourceAttr("data.openstack_compute_servergroup_v2.server_group_1.rules", "max_server_per_host", "3"),
 				),
 			},
 		},
