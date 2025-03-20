f47d2246701a8c8bd41289b4e7b4cd62ab810831
victorcam <victor.camargo@adyen.com>
2025-03-20 14:10:42 +0100

    Fix: importing terraform-plugin-testing

--- openstack/data_source_openstack_dns_zone_share_v2_test.go
+++ openstack/data_source_openstack_dns_zone_share_v2_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 func TestAccDataSourceDNSZoneShareV2_basic(t *testing.T) {
