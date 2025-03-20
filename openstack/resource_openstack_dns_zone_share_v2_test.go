bac701f8b50b91d812dbe141a3a169491a5eb4b5
victorcam <victor.camargo@adyen.com>
2025-03-20 14:10:42 +0100

    Fix: importing terraform-plugin-testing

--- openstack/resource_openstack_dns_zone_share_v2_test.go
+++ openstack/resource_openstack_dns_zone_share_v2_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 
 	"github.com/gophercloud/gophercloud/v2/openstack/dns/v2/zones"
 )
