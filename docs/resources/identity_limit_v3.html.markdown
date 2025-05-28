86522bc4b4cec8ad3a5fe35081f7719962c91597
Parasyris Nikolaos <git@nikparasyr.com>
2025-05-28 16:23:44 +0200

    Add openstack_identity_limit_v3 resource (#1573)
    Add openstack_identity_limit_v3 resource, import test and documentation.
    Limit resource creates only 1 limit, instead of multiple as the api allows.
    The reasoning is that a single limit leads to much easier code to understand
    and maintain, while multiple limits would create issues in handling ids,
    updates etc.

---
+++ docs/resources/identity_limit_v3.html.markdown
@@ -0,0 +1,80 @@
+---
+subcategory: "Identity / Keystone"
+layout: "openstack"
+page_title: "OpenStack: openstack_identity_limit_v3"
+sidebar_current: "docs-openstack-resource-identity-limit-v3"
+description: |-
+  Manages a V3 Limit resource within OpenStack Keystone.
+---
+
+# openstack\_identity\_limit\_v3
+
+Manages a V3 Limit resource within OpenStack Keystone.
+
+~> **Note:** You _must_ have admin privileges in your OpenStack cloud to use
+this resource.
+
+## Example Usage
+
+```hcl
+data "openstack_identity_service_v3" "glance" {
+  name = "glance"
+}
+
+resource "openstack_identity_project_v3" "project_1" {
+  name = "project_1"
+}
+
+resource "openstack_identity_limit_v3" "limit_1" {
+  project_id     = openstack_identity_project_v3.project_1.id
+  service_id     = data.openstack_identity_service_v3.glance.id
+  resource_name  = "image_count_total"
+  resource_limit = 10
+  description    = "foo"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional) The region in which to obtain the V3 Keystone client.
+  If omitted, the `region` argument of the provider is used. Changing this
+  creates a new Limit.
+
+* `project_id` - (Optional) The project the limit applies to. Changing this
+  creates a new Limit.
+
+* `domain_id` - (Optional) The domain the limit applies to. Changing this
+  creates a new Limit.
+
+* `service_id` - (Required) The service the limit applies to. Changing this
+  creates a new Limit.
+
+* `resource_name` - (Required) The resource that the limit applies to. Changing
+  this creates a new Limit.
+
+* `resource_limit` - (Required) Integer for the actual limit.
+
+* `description` - (Optional) Description of the limit.
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `id` - The id of the limit
+* `project_id` - See Argument Reference above.
+* `domain_id` - See Argument Reference above.
+* `region` - See Argument Reference above.
+* `service_id` - See Argument Reference above.
+* `resource_name` - See Argument Reference above.
+* `resource_limit` - See Argument Reference above.
+* `description` - See Argument Reference above.
+
+## Import
+
+Limits can be imported using the `id`, e.g.
+
+```
+$ terraform import openstack_identity_limit_v3.limit_1 89c60255-9bd6-460c-822a-e2b959ede9d2
+```
---
+++ openstack/resource_openstack_identity_limit_v3_test.go
@@ -0,0 +1,160 @@
+package openstack
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+
+	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/limits"
+	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/projects"
+	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/services"
+)
+
+func TestAccIdentityV3Limit_basic(t *testing.T) {
+	var project projects.Project
+	var service services.Service
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckAdminOnly(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckIdentityV3LimitDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityV3LimitBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityV3LimitExists("openstack_identity_limit_v3.limit_1", &project, &service),
+					resource.TestCheckResourceAttrPtr(
+						"openstack_identity_limit_v3.limit_1", "service_id", &service.ID),
+					resource.TestCheckResourceAttrPtr(
+						"openstack_identity_limit_v3.limit_1", "project_id", &project.ID),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_limit_v3.limit_1", "resource_name", "image_count_total"),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_limit_v3.limit_1", "resource_limit", "10"),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_limit_v3.limit_1", "description", "foo"),
+				),
+			},
+			{
+				Config: testAccIdentityV3LimitUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityV3LimitExists("openstack_identity_limit_v3.limit_1", &project, &service),
+					resource.TestCheckResourceAttrPtr(
+						"openstack_identity_limit_v3.limit_1", "service_id", &service.ID),
+					resource.TestCheckResourceAttrPtr(
+						"openstack_identity_limit_v3.limit_1", "project_id", &project.ID),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_limit_v3.limit_1", "resource_name", "image_count_total"),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_limit_v3.limit_1", "resource_limit", "100"),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_limit_v3.limit_1", "description", "bar"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIdentityV3LimitDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+	identityClient, err := config.IdentityV3Client(context.TODO(), osRegionName)
+	if err != nil {
+		return fmt.Errorf("Error creating OpenStack identity client: %s", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "openstack_identity_limit_v3" {
+			continue
+		}
+
+		_, err := limits.Get(context.TODO(), identityClient, rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("Limit still exists")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckIdentityV3LimitExists(n string, project *projects.Project, service *services.Service) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		config := testAccProvider.Meta().(*Config)
+		identityClient, err := config.IdentityV3Client(context.TODO(), osRegionName)
+		if err != nil {
+			return fmt.Errorf("Error creating OpenStack identity client: %s", err)
+		}
+
+		found, err := limits.Get(context.TODO(), identityClient, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("Limit not found")
+		}
+
+		prj, err := projects.Get(context.TODO(), identityClient, found.ProjectID).Extract()
+		if err != nil {
+			return fmt.Errorf("Error retrieving OpenStack project %s: %s", found.ProjectID, err)
+		}
+		*project = *prj
+
+		svc, err := services.Get(context.TODO(), identityClient, found.ServiceID).Extract()
+		if err != nil {
+			return fmt.Errorf("Error retrieving OpenStack service %s: %s", found.ServiceID, err)
+		}
+		*service = *svc
+
+		return nil
+	}
+}
+
+const testAccIdentityV3LimitBasic = `
+data "openstack_identity_service_v3" "glance" {
+  name = "glance"
+}
+
+resource "openstack_identity_project_v3" "project_1" {
+  name = "project_1"
+}
+
+resource "openstack_identity_limit_v3" "limit_1" {
+  project_id = openstack_identity_project_v3.project_1.id
+  service_id = data.openstack_identity_service_v3.glance.id
+  resource_name = "image_count_total"
+  resource_limit = 10
+  description = "foo"
+}
+`
+
+const testAccIdentityV3LimitUpdate = `
+data "openstack_identity_service_v3" "glance" {
+  name = "glance"
+}
+
+resource "openstack_identity_project_v3" "project_1" {
+  name = "project_1"
+}
+
+resource "openstack_identity_limit_v3" "limit_1" {
+  project_id = openstack_identity_project_v3.project_1.id
+  service_id = data.openstack_identity_service_v3.glance.id
+  resource_name = "image_count_total"
+  resource_limit = 100
+  description = "bar"
+}
+`
