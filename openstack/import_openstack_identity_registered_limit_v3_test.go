cf442c635e05af140fe8dd1b769adb434f1a1bf0
Parasyris Nikolaos <git@nikparasyr.com>
2025-06-05 16:20:14 +0200

    Add openstack_identity_registered_limit_v3 resource (#1571)
    Add openstack_identity_registered_limit_v3 resource, import test and documentation.
    Registered Limit resource creates only 1 limit, instead of multiple as the api allows.
    The reasoning is that a single limit leads to much easier code to understand
    and maintain, while multiple limits would create issues in handling ids,
    updates etc.

---
+++ openstack/import_openstack_identity_registered_limit_v3_test.go
@@ -0,0 +1,31 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccIdentityV3RegisteredLimit_importBasic(t *testing.T) {
+	resourceName := "openstack_identity_registered_limit_v3.limit_1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckAdminOnly(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckIdentityV3RegisteredLimitDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityV3RegisteredLimitBasic,
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
---
+++ openstack/resource_openstack_identity_registered_limit_v3_test.go
@@ -0,0 +1,139 @@
+package openstack
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+
+	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/registeredlimits"
+	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/services"
+)
+
+func TestAccIdentityV3RegisteredLimit_basic(t *testing.T) {
+	var service services.Service
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckAdminOnly(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckIdentityV3RegisteredLimitDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityV3RegisteredLimitBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityV3RegisteredLimitExists("openstack_identity_registered_limit_v3.limit_1", &service),
+					resource.TestCheckResourceAttrPtr(
+						"openstack_identity_registered_limit_v3.limit_1", "service_id", &service.ID),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_registered_limit_v3.limit_1", "resource_name", "instances"),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_registered_limit_v3.limit_1", "default_limit", "10"),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_registered_limit_v3.limit_1", "description", "foo"),
+				),
+			},
+			{
+				Config: testAccIdentityV3RegisteredLimitUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityV3RegisteredLimitExists("openstack_identity_registered_limit_v3.limit_1", &service),
+					resource.TestCheckResourceAttrPtr(
+						"openstack_identity_registered_limit_v3.limit_1", "service_id", &service.ID),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_registered_limit_v3.limit_1", "resource_name", "instances"),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_registered_limit_v3.limit_1", "default_limit", "100"),
+					resource.TestCheckResourceAttr(
+						"openstack_identity_registered_limit_v3.limit_1", "description", "bar"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIdentityV3RegisteredLimitDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+	identityClient, err := config.IdentityV3Client(context.TODO(), osRegionName)
+	if err != nil {
+		return fmt.Errorf("Error creating OpenStack identity client: %s", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "openstack_identity_registered_limit_v3" {
+			continue
+		}
+
+		_, err := registeredlimits.Get(context.TODO(), identityClient, rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("Registered limit still exists")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckIdentityV3RegisteredLimitExists(n string, service *services.Service) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		config := testAccProvider.Meta().(*Config)
+		identityClient, err := config.IdentityV3Client(context.TODO(), osRegionName)
+		if err != nil {
+			return fmt.Errorf("Error creating OpenStack identity client: %s", err)
+		}
+
+		found, err := registeredlimits.Get(context.TODO(), identityClient, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("Registered limit not found")
+		}
+
+		svc, err := services.Get(context.TODO(), identityClient, found.ServiceID).Extract()
+		if err != nil {
+			return fmt.Errorf("Error retrieving OpenStack service %s: %s", found.ServiceID, err)
+		}
+
+		*service = *svc
+
+		return nil
+	}
+}
+
+const testAccIdentityV3RegisteredLimitBasic = `
+data "openstack_identity_service_v3" "nova" {
+  name = "nova"
+}
+
+resource "openstack_identity_registered_limit_v3" "limit_1" {
+  service_id = data.openstack_identity_service_v3.nova.id
+  resource_name = "instances"
+  default_limit = 10
+  description = "foo"
+}
+`
+
+const testAccIdentityV3RegisteredLimitUpdate = `
+data "openstack_identity_service_v3" "nova" {
+  name = "nova"
+}
+
+resource "openstack_identity_registered_limit_v3" "limit_1" {
+  service_id = data.openstack_identity_service_v3.nova.id
+  resource_name = "instances"
+  default_limit = 100
+  description = "bar"
+}
+`
