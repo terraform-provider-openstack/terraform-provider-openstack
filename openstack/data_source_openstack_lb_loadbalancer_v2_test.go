f44bbb6aac0f023478dac42e3f7233c9a84732f4
Konstantin Eremin <kod-t@mail.ru>
2025-09-04 20:39:31 +0300

    Remove useless tests, fix depends

--- openstack/data_source_openstack_lb_loadbalancer_v2_test.go
+++ openstack/data_source_openstack_lb_loadbalancer_v2_test.go
@@ -30,11 +30,6 @@ func TestAccDataSourceLBV2LoadBalancer_basic(t *testing.T) {
 						"data.openstack_lb_loadbalancer_v2.lb_ds", "vip_subnet_id"),
 					resource.TestCheckResourceAttrSet(
 						"data.openstack_lb_loadbalancer_v2.lb_ds", "vip_port_id"),
-				),
-			},
-			{
-				Config: testAccDataSourceLbV2LoadBalancerConfigData,
-				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						"data.openstack_lb_loadbalancer_v2.lb_ds", "pools.#", "1"),
 					resource.TestCheckResourceAttrSet(
@@ -43,35 +38,6 @@ func TestAccDataSourceLBV2LoadBalancer_basic(t *testing.T) {
 						"data.openstack_lb_loadbalancer_v2.lb_ds", "listeners.#", "1"),
 					resource.TestCheckResourceAttrSet(
 						"data.openstack_lb_loadbalancer_v2.lb_ds", "listeners.0.id"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccDataSourceLBV2LoadBalancer_secGroup(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-			testAccPreCheckNonAdminOnly(t)
-			testAccPreCheckLB(t)
-		},
-		ProviderFactories: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataSourceLbV2LoadBalancerConfigSecGroup,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"data.openstack_lb_loadbalancer_v2.lb_ds", "name", "loadbalancer_1"),
-					resource.TestCheckResourceAttrSet(
-						"data.openstack_lb_loadbalancer_v2.lb_ds", "vip_port_id"),
-				),
-			},
-			{
-				Config: testAccDataSourceLbV2LoadBalancerConfigData,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"data.openstack_lb_loadbalancer_v2.lb_ds", "security_group_ids.#", "1"),
 				),
 			},
 		},
@@ -94,26 +60,6 @@ func TestAccDataSourceLBV2LoadBalancer_vipNetwork(t *testing.T) {
 						"data.openstack_lb_loadbalancer_v2.lb_ds", "name", "loadbalancer_1"),
 					resource.TestCheckResourceAttrSet(
 						"data.openstack_lb_loadbalancer_v2.lb_ds", "vip_network_id"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccDataSourceLBV2LoadBalancer_vipPortID(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-			testAccPreCheckNonAdminOnly(t)
-			testAccPreCheckLB(t)
-		},
-		ProviderFactories: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataSourceLbV2LoadBalancerConfigVIPPortID,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"data.openstack_lb_loadbalancer_v2.lb_ds", "name", "loadbalancer_1"),
 					resource.TestCheckResourceAttrSet(
 						"data.openstack_lb_loadbalancer_v2.lb_ds", "vip_port_id"),
 				),
@@ -167,46 +113,7 @@ data "openstack_lb_loadbalancer_v2" "lb_ds" {
   loadbalancer_id = openstack_lb_loadbalancer_v2.loadbalancer_1.id
   description = openstack_lb_loadbalancer_v2.loadbalancer_1.description
   vip_address = openstack_lb_loadbalancer_v2.loadbalancer_1.vip_address
-}
-`
-
-const testAccDataSourceLbV2LoadBalancerConfigSecGroup = `
-resource "openstack_networking_secgroup_v2" "secgroup_1" {
-  name = "secgroup_1"
-  description = "secgroup_1"
-}
-
-resource "openstack_networking_network_v2" "network_1" {
-  name = "network_1"
-  admin_state_up = "true"
-}
-
-resource "openstack_networking_subnet_v2" "subnet_1" {
-  name = "subnet_1"
-  network_id = openstack_networking_network_v2.network_1.id
-  cidr = "192.168.199.0/24"
-}
-
-resource "openstack_lb_loadbalancer_v2" "loadbalancer_1" {
-  name = "loadbalancer_1"
-  description = "loadbalancer_1 description"
-  loadbalancer_provider = "octavia"
-  vip_subnet_id = openstack_networking_subnet_v2.subnet_1.id
-  security_group_ids = [
-    openstack_networking_secgroup_v2.secgroup_1.id
-  ]
-
-  timeouts {
-    create = "15m"
-    update = "15m"
-    delete = "15m"
-  }
-}
-
-data "openstack_lb_loadbalancer_v2" "lb_ds" {
-  loadbalancer_id = openstack_lb_loadbalancer_v2.loadbalancer_1.id
-  description = openstack_lb_loadbalancer_v2.loadbalancer_1.description
-  vip_address = openstack_lb_loadbalancer_v2.loadbalancer_1.vip_address
+  depends_on = [ openstack_lb_pool_v2.pool_1 ]
 }
 `
 
@@ -238,55 +145,7 @@ resource "openstack_lb_loadbalancer_v2" "loadbalancer_1" {
 }
 
 data "openstack_lb_loadbalancer_v2" "lb_ds" {
-  loadbalancer_id = openstack_lb_loadbalancer_v2.loadbalancer_1.id
-  vip_address = openstack_lb_loadbalancer_v2.loadbalancer_1.vip_address
-}
-`
-
-const testAccDataSourceLbV2LoadBalancerConfigVIPPortID = `
-resource "openstack_networking_network_v2" "network_1" {
-  name = "network_1"
-  admin_state_up = "true"
-}
-
-resource "openstack_networking_subnet_v2" "subnet_1" {
-  name = "subnet_1"
-  cidr = "192.168.199.0/24"
-  ip_version = 4
-  network_id = openstack_networking_network_v2.network_1.id
-}
-
-resource "openstack_networking_port_v2" "port_1" {
-  name           = "port_1"
-  network_id     = openstack_networking_network_v2.network_1.id
-  admin_state_up = "true"
-  depends_on = ["openstack_networking_subnet_v2.subnet_1"]
-}
-
-resource "openstack_lb_loadbalancer_v2" "loadbalancer_1" {
-  name = "loadbalancer_1"
-  description = "loadbalancer_1 description"
-  loadbalancer_provider = "octavia"
-  vip_port_id = openstack_networking_port_v2.port_1.id
-  depends_on = ["openstack_networking_port_v2.port_1"]
-
-  timeouts {
-    create = "15m"
-    update = "15m"
-    delete = "15m"
-  }
-}
-
-data "openstack_lb_loadbalancer_v2" "lb_ds" {
   name = openstack_lb_loadbalancer_v2.loadbalancer_1.name
-  description = openstack_lb_loadbalancer_v2.loadbalancer_1.description
-}
-`
-
-const testAccDataSourceLbV2LoadBalancerConfigData = `
-data "openstack_lb_loadbalancer_v2" "lb_ds" {
-  loadbalancer_id = openstack_lb_loadbalancer_v2.loadbalancer_1.id
-  description = openstack_lb_loadbalancer_v2.loadbalancer_1.description
   vip_address = openstack_lb_loadbalancer_v2.loadbalancer_1.vip_address
 }
 `
