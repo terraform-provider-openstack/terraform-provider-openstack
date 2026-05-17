7745134badb14dbb5d94b1e721f1e428426dd11b
Mairi Sillars Moya <msillars@cisco.com>
2026-05-18 00:22:59 +0100

    fix: create additional_vips test subnet on separate network

--- openstack/resource_openstack_lb_loadbalancer_v2_test.go
+++ openstack/resource_openstack_lb_loadbalancer_v2_test.go
@@ -544,11 +544,16 @@ resource "openstack_networking_subnet_v2" "subnet_1" {
   network_id = openstack_networking_network_v2.network_1.id
 }
 
+resource "openstack_networking_network_v2" "network_2" {
+  name = "network_2"
+  admin_state_up = "true"
+}
+
 resource "openstack_networking_subnet_v2" "subnet_2" {
   name = "subnet_2"
   cidr = "192.168.200.0/24"
   ip_version = 4
-  network_id = openstack_networking_network_v2.network_1.id
+  network_id = openstack_networking_network_v2.network_2.id
 }
 
 resource "openstack_lb_loadbalancer_v2" "loadbalancer_1" {
